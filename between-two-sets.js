50c7b14efe3e322714235097ec4648ce5509414b
Chad Looker <chad.looker@gmail.com>
2018-01-30 20:13:50 -0500

    Update between-two-sets.js

--- between-two-sets.js
+++ between-two-sets.js
@@ -1,1 +1,14 @@
+// https://www.hackerrank.com/challenges/between-two-sets/problem
 
+function getTotalX(a, b) {
+  return Array.from(Array(b[0])).reduce((aFactors, _, index) => {
+    const i = (index + 1);
+    let flag = true;
+    a.forEach(num => (i % num !== 0) && (flag = false))
+    return flag === true ? aFactors.push(i) && aFactors : aFactors
+  }, []).reduce((answer, aNum) => {
+    let flag = true;
+    b.forEach(bNum => (bNum % aNum !== 0) && (flag = false))
+    return flag === true ? answer.push(aNum) && answer : answer;
+  }, []).length
+}
