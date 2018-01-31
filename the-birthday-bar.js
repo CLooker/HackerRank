be974f8e91a09ae926dc0ce542b35874e99a4eb2
Chad Looker <chad.looker@gmail.com>
2018-01-30 20:19:08 -0500

    Update the-birthday-bar.js

--- the-birthday-bar.js
+++ the-birthday-bar.js
@@ -1,1 +1,11 @@
+// https://www.hackerrank.com/challenges/the-birthday-bar/problem
 
+function solve(n, s, d, m) {
+  return s.reduce((result, item, index) => {
+    let tempSum = 0;
+    for (let i = index; i < index + m; i++) {
+      tempSum = tempSum + s[i];
+    }
+    return tempSum === d ? result = result + 1 : result;
+  }, 0);
+}
