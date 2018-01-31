8cc99e1125e53953da851271ac498b6daa35eb1e
Chad Looker <chad.looker@gmail.com>
2018-01-30 19:59:22 -0500

    Update staircase.js

--- staircase.js
+++ staircase.js
@@ -1,1 +1,15 @@
+// https://www.hackerrank.com/challenges/staircase/problem
 
+function staircase(n) {
+  Array.from(Array(n)).forEach((_, index) => {
+    let temp = "";
+    for (let i = n; i > 0; i--) {
+      if (i >= (n - index)) {
+        temp += '#';
+      } else {
+        temp += " ";
+      }
+    }
+    console.log(temp.split("").reverse().join(""));
+  });
+}
