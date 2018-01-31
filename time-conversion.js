1cdd5bf0bd645c5e2a7436ec544c3831734898a1
Chad Looker <chad.looker@gmail.com>
2018-01-30 20:06:09 -0500

    Update time-conversion.js

--- time-conversion.js
+++ time-conversion.js
@@ -1,1 +1,18 @@
+// https://www.hackerrank.com/challenges/time-conversion/problem
 
+function timeConversion(s) {
+  let convertThis = parseInt(s.slice(0, 2));
+  let thisPartIsGood = s.slice(3, (s.length - 2));
+  if (s.match(/P/g)) {
+    if (convertThis < 12) {
+      convertThis = convertThis + 12
+    }
+  } else if (s.match(/A/g)) {
+    if (convertThis === 12) {
+      convertThis = "00";
+    } else if (convertThis < 10) {
+      convertThis = `0${convertThis}`
+    }
+  }
+  return `${convertThis}:${thisPartIsGood}`;
+}
