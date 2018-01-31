35e8f98b8ec85242e208684f29645a5b92d2a3d8
Chad Looker <chad.looker@gmail.com>
2018-01-30 20:08:00 -0500

    Update grading.js

--- grading.js
+++ grading.js
@@ -1,1 +1,23 @@
+// https://www.hackerrank.com/challenges/grading/problem
 
+function nextFiveMultiple(grade) {
+  let i = grade;
+  do {
+    i++
+  }
+  while (i % 5 !== 0)
+  return i;
+}
+
+function solve(grades) {
+  const example = grades.map(grade => {
+    let nextFiveMultipleReturned = nextFiveMultiple(grade);
+    if (grade < 38) {
+      return grade;
+    } else if ((nextFiveMultipleReturned - grade) < 3) {
+      return nextFiveMultipleReturned;
+    } else if ((nextFiveMultipleReturned - grade) >= 3) {
+      return grade;
+    }
+  }).forEach(grade => console.log(grade))
+}
