75083d12f61523d6496e9d89e162d6c0fe65d041
Chad Looker <chad.looker@gmail.com>
2018-01-30 20:33:36 -0500

    Update the-hurdle-race.js

--- the-hurdle-race.js
+++ the-hurdle-race.js
@@ -1,1 +1,7 @@
+// https://www.hackerrank.com/challenges/the-hurdle-race/problem
 
+function hurdleRace(k, height) {
+  let maxJump = k;
+  height.forEach(h => h > maxJump ? maxJump = h : null);
+  return maxJump - k;
+}
