308f9c08ded2999d6bc953e304553d2f2f99598e
Chad Looker <chad.looker@gmail.com>
2018-01-30 20:11:45 -0500

    Update kangaroo.js

--- kangaroo.js
+++ kangaroo.js
@@ -1,1 +1,25 @@
+// https://www.hackerrank.com/challenges/kangaroo/problem
 
+function kangaroo(x1, v1, x2, v2) {
+  let xLocation = x1;
+  let yLocation = x2;
+  let xJump = v1;
+  let yJump = v2;
+  if ((xLocation > yLocation && xJump >= yJump) || (yLocation > xLocation && yJump >= xJump)) {
+    return "NO";
+  } else {
+    if (xLocation > yLocation) {
+      while (xLocation > yLocation) {
+        xLocation += xJump;
+        yLocation += yJump;
+      }
+      return xLocation === yLocation ? "YES" : "NO";
+    } else if (yLocation > xLocation) {
+      while (yLocation > xLocation) {
+        xLocation += xJump;
+        yLocation += yJump;
+      }
+      return xLocation === yLocation ? "YES" : "NO";
+    }
+  }
+}
