9808b16505d202b6616f23a9b7ed8528e9e8ffbb
Chad Looker <chad.looker@gmail.com>
2018-01-30 20:22:25 -0500

    Update migratory-birds.js

--- migratory-birds.js
+++ migratory-birds.js
@@ -1,1 +1,16 @@
+// https://www.hackerrank.com/challenges/migratory-birds/problem
 
+function migratoryBirds(n, ar) {
+  const example = ar.reduce((result, bird) => {
+    result[bird] = result[bird] + 1;
+    return result;
+  }, {
+    1: 0,
+    2: 0,
+    3: 0,
+    4: 0,
+    5: 0
+  });
+  let most = Object.keys(example).map(key => example[key]).reduce((result, item) => item > result ? result = item : result, 0)
+  return Object.keys(example).filter(key => example[key] === most)[0];
+}
