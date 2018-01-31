4a453be7f68ee86af682c8684a668a8a3a76b66e
Chad Looker <chad.looker@gmail.com>
2018-01-30 20:30:46 -0500

    Update cats-and-a-mouse.js

--- cats-and-a-mouse.js
+++ cats-and-a-mouse.js
@@ -1,1 +1,13 @@
+// https://www.hackerrank.com/challenges/cats-and-a-mouse/problem
 
+function catAndMouse(x, y, z) {
+  let mouseADistance = Math.abs(z - x);
+  let mouseBDistance = Math.abs(z - y);
+  if (mouseADistance < mouseBDistance) {
+    console.log('Cat A');
+  } else if (mouseADistance > mouseBDistance) {
+    console.log('Cat B');
+  } else {
+    console.log('Mouse C');
+  }
+}
