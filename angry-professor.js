0d7be6dcd4b46496e9842241fa297797d4064391
Chad Looker <chad.looker@gmail.com>
2018-01-30 20:39:12 -0500

    Update angry-professor.js

--- angry-professor.js
+++ angry-professor.js
@@ -1,1 +1,8 @@
+// https://www.hackerrank.com/challenges/angry-professor/problem
 
+function angryProfessor(k, a) {
+  return a.reduce((onTimeCounter, t) => {
+    t <= 0 && (onTimeCounter = onTimeCounter + 1)
+    return onTimeCounter;
+  }, 0) < k ? "YES" : "NO";
+}
