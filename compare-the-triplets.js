afae4cecdb205f8bf1590c3e5b6d717599ad5b22
Chad Looker <chad.looker@gmail.com>
2018-01-30 19:50:03 -0500

    Update compare-the-triplets.js

--- compare-the-triplets.js
+++ compare-the-triplets.js
@@ -1,1 +1,22 @@
 
+// https://www.hackerrank.com/challenges/compare-the-triplets/submissions/code/63227776
+
+function solve(a0, a1, a2, b0, b1, b2) {
+  const args = [
+    [a0, b0],
+    [a1, b1],
+    [a2, b2]
+  ]
+  const scoresObj = args.reduce((scoresObj, scorePairArr) => {
+    if (scorePairArr[0] > scorePairArr[1]) {
+      scoresObj.playerA++;
+    } else if (scorePairArr[0] < scorePairArr[1]) {
+      scoresObj.playerB++;
+    }
+    return scoresObj;
+  }, {
+    playerA: 0,
+    playerB: 0
+  })
+  return [scoresObj.playerA, scoresObj.playerB]
+}
