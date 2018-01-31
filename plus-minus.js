fb976eb7a02eec20efa124f59270abea01be9e88
Chad Looker <chad.looker@gmail.com>
2018-01-30 19:57:15 -0500

    Update plus-minus.js

--- plus-minus.js
+++ plus-minus.js
@@ -1,1 +1,19 @@
 
+// https://www.hackerrank.com/challenges/plus-minus/problem
+
+function plusMinus(arr) {
+    const plusMinusCount = arr.reduce((plusMinusCount, n) => {
+        if (n > 0) {
+            plusMinusCount.posCount = plusMinusCount.posCount + 1;
+        } 
+        else if (n < 0) {
+            plusMinusCount.negCount = plusMinusCount.negCount + 1;       
+        } else {
+            plusMinusCount.zeroCount = plusMinusCount.zeroCount + 1;
+        }
+        return plusMinusCount;
+    }, {posCount: 0, negCount: 0, zeroCount: 0});
+    console.log(plusMinusCount.posCount/arr.length);
+    console.log(plusMinusCount.negCount/arr.length);
+    console.log(plusMinusCount.zeroCount/arr.length);
+}
