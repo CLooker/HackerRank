95baf4a1b16cceb5a7f0da36f532ca96b2d8643c
Chad Looker <chad.looker@gmail.com>
2018-01-30 20:20:42 -0500

    Update divisible-sum-pairs.js

--- divisible-sum-pairs.js
+++ divisible-sum-pairs.js
@@ -1,1 +1,16 @@
+// https://www.hackerrank.com/challenges/divisible-sum-pairs/problem
 
+function divisibleSumPairs(n, k, ar) {
+  const targetDivisor = k;
+  return ar.reduce((result, item, index) => {
+    let tempSumList = []
+    for (let i = index + 1; i < ar.length; i++) {
+      let tempSum = item;
+      tempSum = tempSum + ar[i];
+      tempSum % targetDivisor === 0 ?
+        tempSumList.push([index, i]) :
+        null;
+    }
+    return result = result + tempSumList.length;
+  }, 0);
+}
