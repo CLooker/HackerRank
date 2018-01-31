e564c4a73f8599364caba4bf1e581398c5b6b06e
Chad Looker <chad.looker@gmail.com>
2018-01-30 20:26:55 -0500

    Update drawing-book.js

--- drawing-book.js
+++ drawing-book.js
@@ -1,1 +1,16 @@
+// https://www.hackerrank.com/challenges/sock-merchant/problem
 
+function sockMerchant(n, ar) {
+  let total = 0;
+  for (let i = 0; i < ar.length; i++) {
+    let firstSock = ar[i];
+    for (let j = i + 1; j < ar.length; j++) {
+      if (ar[j] === firstSock) {
+        total = total + 1;
+        ar[j] = NaN;
+        break;
+      }
+    }
+  }
+  return total;
+}
