c0d84ca63d4be2043d828ec9a3c51d27292542dd
Chad Looker <chad.looker@gmail.com>
2018-01-30 20:49:07 -0500

    Update repeated-string.js

--- repeated-string.js
+++ repeated-string.js
@@ -1,1 +1,14 @@
+// https://www.hackerrank.com/challenges/repeated-string/problem
 
+function repeatedString(s, n) {
+    const aCount = s.split("").filter(l => l === 'a').length;
+    const x = Math.floor(n / s.length);
+    const totalAs = aCount * x;
+    const leftOverLength = n % s.length;
+    let partial = ""
+    for (let i = 0; i < leftOverLength; i++) {
+        partial += s[i];
+    }
+    const partialAs = partial.split("").filter(l => l === 'a').length;
+    return totalAs + partialAs;
+}
