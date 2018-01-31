dbb48dd3fd44131572127a7b6a42178ff3f229fe
Chad Looker <chad.looker@gmail.com>
2018-01-30 20:40:57 -0500

    Update beautiful-day-at-the-movies.js

--- beautiful-day-at-the-movies.js
+++ beautiful-day-at-the-movies.js
@@ -1,1 +1,17 @@
+// https://www.hackerrank.com/challenges/beautiful-days-at-the-movies/problem
 
+function beautifulDays(i, j, k) {
+  return (
+    Array.from(Array(j - i + 1))
+    .reduce((days, _, ind) => {
+      days.push(ind + i);
+      return days;
+    }, [])
+    .reduce((answer, num, ind) => {
+      let reversed = parseInt(num.toString().split("").reverse().join(""));
+      return Number.isInteger(Math.abs(num - reversed) / k) ?
+        ((answer = answer + 1) && answer) :
+        answer
+    }, 0)
+  );
+}
