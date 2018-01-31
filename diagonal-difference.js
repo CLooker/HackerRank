4474fa7b96de7b3bdd499af3731146f7ea43be7f
Chad Looker <chad.looker@gmail.com>
2018-01-30 19:55:13 -0500

    Update diagonal-difference.js

--- diagonal-difference.js
+++ diagonal-difference.js
@@ -1,1 +1,10 @@
 
+// https://www.hackerrank.com/challenges/diagonal-difference/problem
+
+function diagonalDifference(a) {
+    const primaryDiagonol = a.map((numberArray, index) => numberArray[index]);
+    const secondaryDiagonol = a.reverse().map((numberArray, index) => numberArray[index]);
+    const primaryDiagonolSum = primaryDiagonol.reduce((total, number) => total += number);
+    const secondaryDiagonolSum = secondaryDiagonol.reduce((total, number) => total += number);
+   return Math.abs(primaryDiagonolSum - secondaryDiagonolSum);
+}
