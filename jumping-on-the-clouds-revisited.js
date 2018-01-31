8d0d5c0873ed5f091a08e245d3f42521563a2feb
Chad Looker <chad.looker@gmail.com>
2018-01-30 20:44:38 -0500

    Update jumping-on-the-clouds-revisited.js

--- jumping-on-the-clouds-revisited.js
+++ jumping-on-the-clouds-revisited.js
@@ -1,1 +1,11 @@
+// https://www.hackerrank.com/challenges/jumping-on-the-clouds-revisited/problem
 
+const jumpingOnClouds = (c, k) => (
+    c.reduce(({ energy, ind }, cloud, idx) => (
+        idx === ind
+            ? cloud === 0
+              ? {energy: energy - 1, ind: ind + k}
+              : {energy: energy - 3, ind: ind + k}                
+            : {energy, ind}
+    ), {energy: 100, ind: 0}).energy
+)
