9e9f483f97e83f1fc0e9f121f1a00a0925d6b74b
Chad Looker <chad.looker@gmail.com>
2018-01-30 20:37:28 -0500

    Update utopian-tree.js

--- utopian-tree.js
+++ utopian-tree.js
@@ -1,1 +1,15 @@
+// https://www.hackerrank.com/challenges/utopian-tree/problem
 
+function utopianTree(n) {
+  let height = 1;
+  let spring = (h) => h * 2;
+  let summer = (h) => h + 1;
+  for (let i = 1; i <= n; i++) {
+    if (i % 2 === 0) {
+      height = summer(height);
+    } else {
+      height = spring(height);
+    }
+  }
+  return height;
+}
