aa19b0277cfaef98443906bff96c89f6d9e80223
Chad Looker <chad.looker@gmail.com>
2018-01-30 20:03:41 -0500

    Update birthday-cake-candles.js

--- birthday-cake-candles.js
+++ birthday-cake-candles.js
@@ -1,1 +1,12 @@
+// https://www.hackerrank.com/challenges/birthday-cake-candles/problem
 
+function birthdayCakeCandles(n, ar) {
+  const example = ar.reduce((tallestSumObj, height) => {
+    height >= tallestSumObj.tallest 
+      ? (tallestSumObj.tallest = height, tallestSumObj.sum++) 
+      : null;
+    return tallestSumObj;
+  }, {tallest: 0, sum: 0});
+  const example2 = ar.filter(item => item === example.tallest);
+  return example2.length;
+}
