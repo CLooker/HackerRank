df4e2d859e92a3c44d3dcba2e19128b8979c248f
Chad Looker <chad.looker@gmail.com>
2018-01-30 20:29:33 -0500

    Update electronics-shop.js

--- electronics-shop.js
+++ electronics-shop.js
@@ -1,1 +1,14 @@
+// https://www.hackerrank.com/challenges/electronics-shop/problem
 
+function getMoneySpent(keyboards, drives, s) {
+  let answer = (
+    keyboards
+    .reduce((possibleTotals, k) => {
+      drives.forEach(d => possibleTotals.push(k + d));
+      return possibleTotals
+    }, [])
+    .filter(t => t <= s)
+    .sort((a, b) => b - a)[0]);
+
+  return answer ? answer : -1;
+}
