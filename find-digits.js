8496e4551c6c08451cf1502d34923761d000585b
Chad Looker <chad.looker@gmail.com>
2018-01-30 20:45:55 -0500

    Update find-digits.js

--- find-digits.js
+++ find-digits.js
@@ -1,1 +1,6 @@
 
+// https://www.hackerrank.com/challenges/find-digits/problem
+
+const findDigits = n => (
+  n.toString().split("").map(digit => n % digit === 0 ? 1 : 0).reduce((sum, item) => sum = sum + item)
+)
