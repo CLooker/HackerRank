0b3f0bf6b972f326471448bcb423a84f3bd9851f
Chad Looker <chad.looker@gmail.com>
2018-01-30 20:47:53 -0500

    Update cutting-the-sticks.js

--- cutting-the-sticks.js
+++ cutting-the-sticks.js
@@ -1,1 +1,38 @@
+// https://www.hackerrank.com/challenges/cut-the-sticks/problem
 
+// declarative, immutable, recursive
+const handleSticks = ({ mutableSticksArr, sticksLeftPerTurn, shortestStick }) => {
+  const shortestStickReturned = shortestStick(mutableSticksArr);
+  return mutableSticksArr.length > 0 
+    ?   handleSticks({
+            mutableSticksArr: mutableSticksArr
+                .map(stick => stick - shortestStickReturned)
+                .filter(stick => stick > 0),
+            sticksLeftPerTurn: [...sticksLeftPerTurn, mutableSticksArr.length],
+            shortestStick
+        }) 
+    :   sticksLeftPerTurn
+}
+const cutTheSticks = arr => (   
+    handleSticks({
+        mutableSticksArr: [...arr],
+        sticksLeftPerTurn: [],
+        shortestStick: (stickArr) => stickArr.slice(0).sort((a, b) => a - b)[0]
+    })
+)
+
+// imperative, immutable
+const cutTheSticks = arr => {
+    let mutableSticksArr = Object.assign([], arr);
+    let shortestStick = (stickArr) => stickArr.sort((a, b) => a - b)[0];
+    let sticksLeftPerTurn = [];
+    do {
+        sticksLeftPerTurn = [...sticksLeftPerTurn, mutableSticksArr.length];
+        shortestStickReturned = shortestStick(mutableSticksArr);
+        mutableSticksArr = mutableSticksArr
+            .map(stick => stick - shortestStickReturned)
+            .filter(stick => stick > 0)
+    }
+    while(mutableSticksArr.length > 0)
+    return sticksLeftPerTurn;
+}
