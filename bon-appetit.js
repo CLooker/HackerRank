bfd22c72f86c1b0eee5054a436ea91cb208e1cdd
Chad Looker <chad.looker@gmail.com>
2018-02-01 17:21:58 -0500

    Update bon-appetit.js

--- bon-appetit.js
+++ bon-appetit.js
@@ -1,20 +1,9 @@
 // https://www.hackerrank.com/challenges/bon-appetit/problem
 
 function bonAppetit(n, k, b, ar) {
-  const indexOfNotEaten = k;
-  const actuallyCharged = b;
-  const food = ar;
-  const difference = actuallyCharged - (ar.reduce((result, item, index) => {
-    if (index !== indexOfNotEaten) {
-      result = result + item;
-    }
-    return result;
-  }, 0) / 2)
-
-  if (difference === 0) {
-    return "Bon Appetit"
-  } else {
-    return difference
-  }
-
+  const difference = b - (ar.reduce((result, item, index) => (
+   index !== k ? result += item : result
+  ), 0) / 2)
+  
+  return difference === 0 ? "Bon Appetit" : difference
 }
