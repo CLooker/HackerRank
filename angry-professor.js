6e50d2978277ac5f061f7a7933860c2c91fbab64
Chad Looker <chad.looker@gmail.com>
2018-02-01 08:49:57 -0500

    Update angry-professor.js

--- angry-professor.js
+++ angry-professor.js
@@ -1,8 +1,5 @@
 // https://www.hackerrank.com/challenges/angry-professor/problem
 
-function angryProfessor(k, a) {
-  return a.reduce((onTimeCounter, t) => {
-    t <= 0 && (onTimeCounter = onTimeCounter + 1)
-    return onTimeCounter;
-  }, 0) < k ? "YES" : "NO";
-}
+const angryProfessor = (k, a) => (
+  a.reduce((onTimeCounter, t) => (t <= 0 ? ++onTimeCounter : onTimeCounter), 0) < k ? "YES" : "NO"
+)
