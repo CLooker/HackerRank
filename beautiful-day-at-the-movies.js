cebe43c60c747989d22ecbd572fe8693629f12f1
Chad Looker <chad.looker@gmail.com>
2018-02-01 10:57:58 -0500

    Update beautiful-day-at-the-movies.js

--- beautiful-day-at-the-movies.js
+++ beautiful-day-at-the-movies.js
@@ -4,8 +4,8 @@ const beautifulDays = (i, j, k) => (
   Array.from(Array(j - i + 1))
   .reduce((days, _, ind) => days.push(ind + i) && days, [])
   .reduce((answer, num, ind) => (
-    Number.isInteger(Math.abs(num - parseInt(num.toString().split("").reverse().join(""))) / k) ?
-    ++answer :
-    answer
+    Number.isInteger(Math.abs(num - parseInt(num.toString().split("").reverse().join(""))) / k) 
+      ? ++answer 
+      : answer
   ), 0)
 )
