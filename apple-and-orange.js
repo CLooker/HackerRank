2f6eada4b17aeddb4c16ad5d670f613915b96267
Chad Looker <chad.looker@gmail.com>
2018-02-01 08:53:18 -0500

    Update apple-and-orange.js

--- apple-and-orange.js
+++ apple-and-orange.js
@@ -1,7 +1,8 @@
 // https://www.hackerrank.com/challenges/apple-and-orange/problem
 
-function appleAndOrange(s, t, a, b, apple, orange) {
-  let aOnRoof = apple.filter(app => app + a >= s && app + a <= t);
-  let oOnRoof = orange.filter(or => b + or <= t && b + or >= s);
-  return [aOnRoof.length, oOnRoof.length]
-}
+const appleAndOrange = (s, t, a, b, apple, orange) => (
+  [
+    apple.filter(app => app + a >= s && app + a <= t).length, 
+    orange.filter(or => b + or <= t && b + or >= s).length
+  ]
+)
