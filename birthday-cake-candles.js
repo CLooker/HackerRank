81a93d0a43f48965c18830e80288be4dbec26e7e
Chad Looker <chad.looker@gmail.com>
2018-02-01 11:44:30 -0500

    Update birthday-cake-candles.js

--- birthday-cake-candles.js
+++ birthday-cake-candles.js
@@ -1,13 +1,12 @@
 // https://www.hackerrank.com/challenges/birthday-cake-candles/problem
 
 function birthdayCakeCandles(n, ar) {
-  const example = ar.reduce((tallestSumObj, height) => {
-    height >= tallestSumObj.tallest 
-      ? (tallestSumObj.tallest = height, tallestSumObj.sum++) 
-      : null;
-    return tallestSumObj;
-  }, {tallest: 0, sum: 0});
-  return ar.filter(item => item === example.tallest).legnth;
+  const tallest = ar.reduce(({ tallest, sum }, height) => (
+    height >= tallest 
+      ? {tallest: height, sum: ++sum}
+      : {tallest, sum}
+  ), {tallest: 0, sum: 0}).tallest;
+  return ar.filter(height => height === tallest).length;
 }
 
 // same as above, but sexier, but probably not as readable
