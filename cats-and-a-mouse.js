ec7b72bfee1168236f3000de5af53b0d07861c9b
Chad Looker <chad.looker@gmail.com>
2018-02-01 18:39:15 -0500

    Update cats-and-a-mouse.js

--- cats-and-a-mouse.js
+++ cats-and-a-mouse.js
@@ -16,14 +16,13 @@ function catAndMouse(x, y, z) {
 }
 
 // fancy
-const catAndMouse = (x, y, z) => (
-    [null]
-      .reduce(({ catX, catY, answer }, _, index) => (
-        catX < catY
-            ? {catX, catY, answer: answer.concat('Cat A')}  
-            : catX > catY
-                ? {catX, catY, answer: answer.concat('Cat B')}
-                : {catX, catY, answer: answer.concat('Mouse C')}
-      ), {catX: Math.abs(z - x),  catY: Math.abs(z - y), answer: []})
-      .answer
-)
+const catAndMouse = (x, y, z) =>
+  [null].reduce(
+    ({ catX, catY, answer }, _, index) =>
+      catX < catY
+        ? { catX, catY, answer: answer.concat("Cat A") }
+        : catX > catY
+          ? { catX, catY, answer: answer.concat("Cat B") }
+          : { catX, catY, answer: answer.concat("Mouse C") },
+    { catX: Math.abs(z - x), catY: Math.abs(z - y), answer: [] }
+  ).answer;
