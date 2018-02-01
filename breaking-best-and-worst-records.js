7b4bc974cef569de4d51a4dffeb3d9718844b268
Chad Looker <chad.looker@gmail.com>
2018-02-01 18:08:30 -0500

    Update breaking-best-and-worst-records.js

--- breaking-best-and-worst-records.js
+++ breaking-best-and-worst-records.js
@@ -1,16 +1,14 @@
 // https://www.hackerrank.com/challenges/breaking-best-and-worst-records/problem
 
-function breakingRecords(scores) {
-  let e = scores.reduce(({ low, lowCounter, high, highCounter }, s, index) => {
+const breakingRecords = scores => (
+  scores.reduce(({ high, low, answer }, s, index) => {
      if (index === 0) {low = s; high = s}
-     if (s < low) {low = s; lowCounter += 1}
-     if (s > high) {high = s; highCounter += 1}
-     return {low, lowCounter, high, highCounter};
+     if (s < low) {low = s; ++answer[1]}
+     if (s > high) {high = s; ++answer[0]}
+     return {low, high, answer};
   }, {
-    low: 0,
-    lowCounter: 0,
     high: 0,
-    highCounter: 0
-  });
-  return [e.highCounter, e.lowCounter];
-}
+    low: 0,    
+    answer: [0, 0]
+  }).answer
+)
