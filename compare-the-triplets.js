da810fec97c4d4b1b7028a2bbf2464c5e9521c11
Chad Looker <chad.looker@gmail.com>
2018-02-01 18:58:05 -0500

    Update compare-the-triplets.js

--- compare-the-triplets.js
+++ compare-the-triplets.js
@@ -14,13 +14,13 @@ const solve = (a0, a1, a2, b0, b1, b2) =>
 // regular
 function solve(a0, a1, a2, b0, b1, b2) {
   const args = [[a0, b0], [a1, b1], [a2, b2]];
-  const scoresObj = args.reduce((scoresObj, scorePairArr) => {
+  const scoresObj = args.reduce(({ playerA, playerB }, scorePairArr) => {
     if (scorePairArr[0] > scorePairArr[1]) {
-      scoresObj.playerA++;
+      ++playerA;
     } else if (scorePairArr[0] < scorePairArr[1]) {
-      scoresObj.playerB++;
+      ++playerB;
     }
-    return scoresObj;
+    return {playerA, playerB};
   }, {
     playerA: 0,
     playerB: 0
