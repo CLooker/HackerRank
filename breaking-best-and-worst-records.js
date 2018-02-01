5e0ebbdb69ac83757e1a1cdafc3012102d320157
Chad Looker <chad.looker@gmail.com>
2018-02-01 18:14:05 -0500

    Update breaking-best-and-worst-records.js

--- breaking-best-and-worst-records.js
+++ breaking-best-and-worst-records.js
@@ -2,7 +2,7 @@
 
 const breakingRecords = scores => (
   scores.reduce(({ high, low, answer }, s, index) => {
-     if (index === 0) {high = s; low = s}
+     if (index === 0) {return {high: s, low: s, answer}}
      if (s > high) {high = s; ++answer[0]}
      if (s < low) {low = s; ++answer[1]}
      return {low, high, answer};
