cbabefb59a62f56e0f68c7bf806b8b71069f4669
Chad Looker <chad.looker@gmail.com>
2018-02-01 11:17:01 -0500

    Update between-two-sets.js

--- between-two-sets.js
+++ between-two-sets.js
@@ -1,14 +1,15 @@
 // https://www.hackerrank.com/challenges/between-two-sets/problem
 
-function getTotalX(a, b) {
-  return Array.from(Array(b[0])).reduce((aFactors, _, index) => {
-    const i = (index + 1);
-    let flag = true;
-    a.forEach(num => (i % num !== 0) && (flag = false))
-    return flag === true ? aFactors.push(i) && aFactors : aFactors
-  }, []).reduce((answer, aNum) => {
-    let flag = true;
-    b.forEach(bNum => (bNum % aNum !== 0) && (flag = false))
-    return flag === true ? answer.push(aNum) && answer : answer;
-  }, []).length
-}
+const getTotalX = (a, b) => (
+  Array.from(Array(b[0]))
+    .reduce((aFactors, _, index) => (
+      a.reduce((flag, num) => (((index + 1) % num !== 0) && (flag = false)) || flag, true) === true 
+        ? aFactors.concat(index + 1) 
+        : aFactors
+    ), [])
+    .reduce((answer, aNum) => (
+      b.reduce((flag, bNum) => ((bNum % aNum !== 0) && (flag = false)) || flag, true) === true 
+        ? answer.concat(aNum) 
+        : answer
+    ), []).length
+)
