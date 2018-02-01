e8e263d839949aabdb259bff8354b392b69d20c4
Chad Looker <chad.looker@gmail.com>
2018-02-01 11:41:34 -0500

    Update birthday-cake-candles.js

--- birthday-cake-candles.js
+++ birthday-cake-candles.js
@@ -7,6 +7,20 @@ function birthdayCakeCandles(n, ar) {
       : null;
     return tallestSumObj;
   }, {tallest: 0, sum: 0});
-  const example2 = ar.filter(item => item === example.tallest);
-  return example2.length;
+  return ar.filter(item => item === example.tallest).legnth;
 }
+
+// same as above, but sexier, but probably not as readable
+
+const birthdayCakeCandles = (n, ar) => (
+  ar.reduce(({ tallest, candles }, height) => (
+    height === tallest 
+      ? {tallest, candles: ++candles} 
+      : {tallest, candles}
+  ), {
+        tallest: ar.reduce(({ tallest, sum }, height) => (
+                     height >= tallest ? {tallest: height, sum: ++sum} : {tallest, sum}
+                   ), {tallest: 0, sum: 0}).tallest,
+        candles: 0
+      }).candles
+)
