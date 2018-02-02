346a1992591692d41a57a262964a8280cff3f080
Chad Looker <chad.looker@gmail.com>
2018-02-02 10:31:26 -0500

    Update jumping-on-the-clouds.js

--- jumping-on-the-clouds.js
+++ jumping-on-the-clouds.js
@@ -1,24 +1,25 @@
 // https://www.hackerrank.com/challenges/jumping-on-the-clouds/problem
 
 // declarative, immutable 
-const jumpingOnClouds = c => (c.reduce(({ jumps, i }, _, index) => (
-  index === i 
-    ? c[index + 2] === 0 
-      ? {jumps: ++jumps, i: i += 2} 
-      : {jumps: ++jumps, i: ++i} 
-    : {jumps, i}), {jumps: -1, i: 0}).jumps
+const jumpingOnClouds = c => (
+  c.reduce(({ jumps, i }, _, index) => (
+    index === i 
+      ? c[index + 2] === 0 
+        ? {jumps: ++jumps, i: i += 2} 
+        : {jumps: ++jumps, i: ++i} 
+      : {jumps, i}), {jumps: -1, i: 0}).jumps
 )
 
 // imperative
 function jumpingOnClouds(c) {
-  let jumps = 0;
+  let jumps = -1;
   for (let i = 0; i < c.length;) {
     if (c[i + 2] === 0) {
       i += 2;
     } else {
       i += 1;
     }
-    jumps += 1;
+    ++jumps;
   }
-  return --jumps;
+  return jumps;
 }
