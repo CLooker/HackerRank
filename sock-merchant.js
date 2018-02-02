243d2f86d92417b328c9ea00151de2d7e598429f
Chad Looker <chad.looker@gmail.com>
2018-02-02 08:41:48 -0500

    Update sock-merchant.js

--- sock-merchant.js
+++ sock-merchant.js
@@ -1,16 +1,14 @@
 // https://www.hackerrank.com/challenges/sock-merchant/problem
 
-function sockMerchant(n, ar) {
-  let total = 0;
-  for (let i = 0; i < ar.length; i++) {
-    let firstSock = ar[i];
+const sockMerchant = (n, ar) => (
+  ar.reduce((total, sock, i) => {
+    const firstSock = sock;
     for (let j = i + 1; j < ar.length; j++) {
       if (ar[j] === firstSock) {
-        total = total + 1;
-        ar[j] = NaN;
+        (++total) && (ar[j] = NaN)
         break;
       }
     }
-  }
-  return total;
-}
+    return total;
+  }, 0)
+)
