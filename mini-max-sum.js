feff46765463ad3715da93312b62ea5912f51735
Chad Looker <chad.looker@gmail.com>
2018-02-02 14:34:07 -0500

    Update mini-max-sum.js

--- mini-max-sum.js
+++ mini-max-sum.js
@@ -1,16 +1,24 @@
 // https://www.hackerrank.com/challenges/mini-max-sum/problem
 
 function miniMaxSum(arr) {
-  const maxAndMin = arr.reduce((newArr, n, index) => {
-    return newArr.concat(arr.filter((num, ind) => index !== ind).reduce((total, curr) => total += curr, 0));
-  }, []).reduce((maxAndMin, n, index) => {
-    index === 0 ? (maxAndMin.max = n, maxAndMin.min = n) : null;
-    n > maxAndMin.max ? maxAndMin.max = n : null;
-    n < maxAndMin.min ? maxAndMin.min = n : null;
-    return maxAndMin;
-  }, {
-    min: null,
-    max: null
-  });
+  const maxAndMin = arr
+    .reduce(
+      (newArr, n, index) =>
+        newArr.concat(
+          arr
+            .filter((num, ind) => index !== ind)
+            .reduce((total, curr) => (total += curr), 0)
+        ),
+      []
+    )
+    .reduce(
+      ({ min, max }, n, index) => {
+         if (index === 0) {return {min: n, max: n}};
+         (n > max) && (max = n);
+         (n < min) && (min = n);
+         return {min, max};
+      },
+      { min: null, max: null }
+    );
   console.log(`${maxAndMin.min} ${maxAndMin.max}`);
 }
