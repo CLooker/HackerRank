aae5ea82c00f6dc981828005e9ac6f3298b1ee30
Chad Looker <chad.looker@gmail.com>
2018-02-02 09:37:18 -0500

    Update drawing-book.js

--- drawing-book.js
+++ drawing-book.js
@@ -1,20 +1,20 @@
-// https://www.hackerrank.com/challenges/drawing-book/problem
+// https://www.hackerrank.com/challenges/drawing-book/problem// https://www.hackerrank.com/challenges/drawing-book/problem
 
 function solve(n, p){
-    let fromFront = () => {
+    const fromFront = (n, p) => {
         let pages = [0, 1];
         let turnCounter = 0;
         if (pages[0] !== p && pages[1] !== p) {
             do {
                 pages = [pages[0] + 2, pages[1] + 2];
-                turnCounter = turnCounter + 1;
+                ++turnCounter;
             }
             while (pages[0] !== p && pages[1] !== p)
         }       
         return turnCounter; 
     }
-    let fromBack = () => {        
-        let setPages = (pg) => pg % 2 === 0 ? [pg, pg+1] : [pg-1, pg];
+    const fromBack = (n, p) => {        
+        const setPages = (pg) => pg % 2 === 0 ? [pg, pg+1] : [pg-1, pg];
         let pages = setPages(n);
         let turnCounter = 0;
         if (pages[0] !== p && pages[1] !== p) {
@@ -26,6 +26,5 @@ function solve(n, p){
         }       
         return turnCounter;        
     }
-    return [ fromFront(), fromBack() ]
-        .sort((a, b) => a - b)[0];
+    return [ fromFront(n, p), fromBack(n, p) ].sort((a, b) => a - b)[0];
 }
