3e3ddacb13203bb9e48c891099f9306149aacfa6
Chad Looker <chad.looker@gmail.com>
2018-02-02 13:39:26 -0500

    Update migratory-birds.js

--- migratory-birds.js
+++ migratory-birds.js
@@ -1,16 +1,11 @@
 // https://www.hackerrank.com/challenges/migratory-birds/problem
 
 function migratoryBirds(n, ar) {
-  const example = ar.reduce((result, bird) => {
-    result[bird] = result[bird] + 1;
-    return result;
-  }, {
-    1: 0,
-    2: 0,
-    3: 0,
-    4: 0,
-    5: 0
-  });
-  let most = Object.keys(example).map(key => example[key]).reduce((result, item) => item > result ? result = item : result, 0)
-  return Object.keys(example).filter(key => example[key] === most)[0];
+  const bPopByType = ar.reduce(
+    (bPopByType, type) => (
+      Object.assign({}, bPopByType, ++bPopByType[type])
+    ), {1: 0, 2: 0, 3: 0, 4: 0, 5: 0});
+  const bPopTypes = Object.keys(bPopByType);
+  const most = bPopTypes.map(type => bPopByType[type]).reduce((result, pop) => pop > result ? result = pop : result, null);
+  return bPopTypes.filter(type => bPopByType[type] === most)[0];
 }
