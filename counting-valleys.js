666da7c355463d4fd80e91ba46213a83398a63f4
Chad Looker <chad.looker@gmail.com>
2018-02-01 19:03:38 -0500

    Update counting-valleys.js

--- counting-valleys.js
+++ counting-valleys.js
@@ -1,11 +1,12 @@
 // https://www.hackerrank.com/challenges/counting-valleys/problem
 
-const countingValleys = (n, s) => (
-  s.split("")
-  .reduce(({ pos, vSteps, valleys }, step) => {
-    step === 'U' ? ++pos : --pos;
-    (pos < 0) && ++vSteps;
-    (pos === 0) && (vSteps > 0) && (++valleys) && (vSteps = 0);
-    return {pos, vSteps, valleys};
-  }, {pos: 0, vSteps: 0, valleys: 0}).valleys
-)
+const countingValleys = (n, s) =>
+  s.split("").reduce(
+    ({ pos, vSteps, valleys }, step) => {
+      step === "U" ? ++pos : --pos;
+      pos < 0 && ++vSteps;
+      pos === 0 && vSteps > 0 && ++valleys && (vSteps = 0);
+      return { pos, vSteps, valleys };
+    },
+    { pos: 0, vSteps: 0, valleys: 0 }
+  ).valleys;
