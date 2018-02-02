308f8c123470dfdb22906c97f59ca17b17f8948c
Chad Looker <chad.looker@gmail.com>
2018-02-02 08:45:29 -0500

    Update drawing-book.js

--- drawing-book.js
+++ drawing-book.js
@@ -1,16 +1,31 @@
 // https://www.hackerrank.com/challenges/sock-merchant/problem
 
-function sockMerchant(n, ar) {
-  let total = 0;
-  for (let i = 0; i < ar.length; i++) {
-    let firstSock = ar[i];
-    for (let j = i + 1; j < ar.length; j++) {
-      if (ar[j] === firstSock) {
-        total = total + 1;
-        ar[j] = NaN;
-        break;
-      }
+function solve(n, p){
+    let fromFront = () => {
+        let pages = [0, 1];
+        let turnCounter = 0;
+        if (pages[0] !== p && pages[1] !== p) {
+            do {
+                pages = [pages[0] + 2, pages[1] + 2];
+                turnCounter = turnCounter + 1;
+            }
+            while (pages[0] !== p && pages[1] !== p)
+        }       
+        return turnCounter; 
     }
-  }
-  return total;
+    let fromBack = () => {        
+        let setPages = (pg) => pg % 2 === 0 ? [pg, pg+1] : [pg-1, pg];
+        let pages = setPages(n);
+        let turnCounter = 0;
+        if (pages[0] !== p && pages[1] !== p) {
+            do {
+                pages = setPages(pages[1] - 2);
+                turnCounter = turnCounter + 1;
+            }
+            while (pages[0] !== p && pages[1] !== p)
+        }       
+        return turnCounter;        
+    }
+    return [ fromFront(), fromBack() ]
+        .sort((a, b) => a - b)[0];
 }
