e33a81c0cd728944d3d0c8bcb9388889c99450b1
Chad Looker <chad.looker@gmail.com>
2018-02-04 11:03:11 -0500

    Update equality-in-a-array.js

--- equality-in-a-array.js
+++ equality-in-a-array.js
@@ -1,29 +1,27 @@
 // https://www.hackerrank.com/challenges/equality-in-a-array/problem
 
+// imperative
 function equalizeArray(arr) {
-    let coll = [];
-    for (let i = 0; i < arr.length; i++) {
-        const target = arr[i];
-        let tempColl = [target];
-        for (let j = 0; j < arr.length; j++) {
-            if (j != i) {
-                if (arr[j] === target) {
-                    tempColl.push(arr[j])
-                }
-                coll.push(tempColl)
-            }
-        }
+  let coll = [];
+  for (let i = 0; i < arr.length; i++) {
+    let tempColl = [];
+    for (let j = 0; j < arr.length; j++) {
+      if (arr[j] === arr[i]) {
+        tempColl.push(arr[j]);
+      }
+      coll.push(tempColl);
     }
-    let biggestTempColl = 0;
-    coll.forEach(tempColl => {
-        if (tempColl.length > biggestTempColl) {
-            biggestTempColl = tempColl.length;
-        }
-    })
-    return arr.length - biggestTempColl;
+  }
+  let biggestTempColl = 0;
+  for (let i = 0; i < coll.length; i++) {
+    if (coll[i].length > biggestTempColl) {
+      biggestTempColl = coll[i].length;
+    }
+  }
+  return arr.length - biggestTempColl;
 }
 
-// probably not as readable, but immutable, implicit returns
+// probably not as readable, but declarative, immutable, implicit returns
 const equalizeArray = arr =>
   arr.length -
   Array.from(Array(arr.length))
