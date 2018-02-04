1dbf05d65c32f91e3d903ad3c8bf7cda5fb8d780
Chad Looker <chad.looker@gmail.com>
2018-02-04 17:32:45 -0500

    Update plus-minus.js

--- plus-minus.js
+++ plus-minus.js
@@ -2,18 +2,34 @@
 // https://www.hackerrank.com/challenges/plus-minus/problem
 
 function plusMinus(arr) {
-    const plusMinusCount = arr.reduce((plusMinusCount, n) => {
-        if (n > 0) {
-            plusMinusCount.posCount = plusMinusCount.posCount + 1;
-        } 
-        else if (n < 0) {
-            plusMinusCount.negCount = plusMinusCount.negCount + 1;       
-        } else {
-            plusMinusCount.zeroCount = plusMinusCount.zeroCount + 1;
-        }
-        return plusMinusCount;
-    }, {posCount: 0, negCount: 0, zeroCount: 0});
-    console.log(plusMinusCount.posCount/arr.length);
-    console.log(plusMinusCount.negCount/arr.length);
-    console.log(plusMinusCount.zeroCount/arr.length);
+  const plusMinusCount = arr.reduce(
+    (plusMinusCount, n) => {
+      if (n > 0) {
+        ++plusMinusCount.posCount;
+      } else if (n < 0) {
+        ++plusMinusCount.negCount;
+      } else {
+        ++plusMinusCount.zeroCount;
+      }
+      return plusMinusCount;
+    },
+    { posCount: 0, negCount: 0, zeroCount: 0 }
+  );
+  console.log(plusMinusCount.posCount / arr.length);
+  console.log(plusMinusCount.negCount / arr.length);
+  console.log(plusMinusCount.zeroCount / arr.length);
+}
+
+const plusMinus = arr => {
+  const plusMinusCount = arr.reduce(
+    ({ posCount, negCount, zeroCount }, n) =>
+      n > 0
+        ? { posCount: ++posCount, negCount, zeroCount }
+        : n < 0
+          ? { posCount, negCount: ++negCount, zeroCount }
+          : { posCount, negCount, zeroCount: ++zeroCount },
+    { posCount: 0, negCount: 0, zeroCount: 0 }
+  );
+    
+  Object.keys(plusMinusCount).forEach(key => console.log(plusMinusCount[key] / arr.length))
 }
