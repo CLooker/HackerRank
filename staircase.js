c741cea4b0819b53cccdd6de8bda12b56013ba1b
Chad Looker <chad.looker@gmail.com>
2018-02-07 19:57:36 -0500

    Update staircase.js

--- staircase.js
+++ staircase.js
@@ -1,5 +1,31 @@
 // https://www.hackerrank.com/challenges/staircase/problem
 
+// declarative, immutable, implicit returns, side-effects pushed to edge
+const staircase = n =>
+  Array.from(Array(n))
+    .reduce(
+      ({ stairs }, _, index) => ({
+        stairs: [
+          ...stairs,
+          Array.from(Array(n)).reduce(
+            ({ stair, k }, _, j) =>
+              k >= n - index
+                ? { stair: (stair += "#"), k: --k }
+                : { stair: (stair += " "), k: --k },
+            { stair: "", k: n }
+          ).stair
+        ]
+      }),
+      { stairs: [] }
+    )
+    .stairs.map(stair =>
+      stair
+        .split("")
+        .reverse()
+        .join("")
+    )
+    .forEach(stair => console.log(stair));
+
 function staircase(n) {
   Array.from(Array(n)).forEach((_, index) => {
     let temp = "";
