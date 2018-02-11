375df512863a163b86090ce78e5b80c8622857f7
Chad Looker <chad.looker@gmail.com>
2018-02-11 11:06:23 -0500

    Update utopian-tree.js

--- utopian-tree.js
+++ utopian-tree.js
@@ -1,14 +1,21 @@
 // https://www.hackerrank.com/challenges/utopian-tree/problem
 
+// declarative, immutable, implicit return
+const utopianTree = n =>
+  n > 0
+    ? Array.from(Array(n))
+        .map((_, i) => ++i)
+        .reduce((h, i) => (i % 2 === 0 ? ++h : h * 2), 1)
+    : 1;
+
+// imperative, mutable, explicit return
 function utopianTree(n) {
   let height = 1;
-  let spring = (h) => h * 2;
-  let summer = (h) => h + 1;
   for (let i = 1; i <= n; i++) {
     if (i % 2 === 0) {
-      height = summer(height);
+      ++height;
     } else {
-      height = spring(height);
+      height = height * 2;
     }
   }
   return height;
