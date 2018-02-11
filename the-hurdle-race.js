66c5ac151da4483f3746b09317c64ec35553d28e
Chad Looker <chad.looker@gmail.com>
2018-02-11 11:04:30 -0500

    Update the-hurdle-race.js

--- the-hurdle-race.js
+++ the-hurdle-race.js
@@ -1,7 +1,16 @@
 // https://www.hackerrank.com/challenges/the-hurdle-race/problem
 
+// declarative, immutable, implicit return
+const hurdleRace = (k, height) =>
+  height.reduce((maxJump, h) => (h > maxJump ? h : maxJump), k) - k;
+
+// imperative, mutable, explicit return
 function hurdleRace(k, height) {
   let maxJump = k;
-  height.forEach(h => h > maxJump ? maxJump = h : null);
+  for (let i = 0; i < height.length; i++) {
+    if (height[i] > maxJump) {
+      maxJump = height[i];
+    }
+  }
   return maxJump - k;
 }
