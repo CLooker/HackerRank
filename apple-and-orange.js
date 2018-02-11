155fb41192582d772be40dd0f2a7a6a2092310f4
Chad Looker <chad.looker@gmail.com>
2018-02-11 11:01:25 -0500

    Update apple-and-orange.js

--- apple-and-orange.js
+++ apple-and-orange.js
@@ -1,8 +1,25 @@
 // https://www.hackerrank.com/challenges/apple-and-orange/problem
 
-const appleAndOrange = (s, t, a, b, apple, orange) => (
+// declarative, immutable, side-effects pushed to edge
+const countApplesAndOranges = (s, t, a, b, apple, orange) =>
   [
-    apple.filter(app => app + a >= s && app + a <= t).length, 
+    apple.filter(app => app + a >= s && app + a <= t).length,
     orange.filter(or => b + or <= t && b + or >= s).length
-  ]
-)
+  ].forEach(count => console.log(count));
+
+// imperative, mutable
+function countApplesAndOranges(s, t, a, b, apples, oranges) {
+   let aCount = 0;
+   for (let i = 0; i < apple.length; i++) {
+       if (    ((apple[i] + a) >= s) &&   ( (apple[i] + a) <= t) ) {
+           ++aCount;
+       }
+   }
+   let oCount = 0;
+   for (let i = 0; i < orange.length; i++) {
+       if ( (orange[i] + b <= t) && (b + orange[i] >= s)    ) {
+          ++oCount; 
+       }
+   }
+   console.log(`${aCount}\n${oCount}`);
+}
