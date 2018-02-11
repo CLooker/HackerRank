f3ef500d88e7880ff6442d7bd85e16d181456ecd
Chad Looker <chad.looker@gmail.com>
2018-02-11 11:05:35 -0500

    Update time-conversion.js

--- time-conversion.js
+++ time-conversion.js
@@ -1,17 +1,45 @@
 // https://www.hackerrank.com/challenges/time-conversion/problem
 
+// clean, but not so readable
 function timeConversion(s) {
   let convertThis = parseInt(s.slice(0, 2));
-  let thisPartIsGood = s.slice(3, (s.length - 2));
+  const thisPartIsGood = s.slice(3, s.length - 2);
+  s.match(/P/g)
+    ? convertThis < 12 && (convertThis += 12)
+    : s.match(/A/g) && convertThis === 12
+      ? (convertThis = "00")
+      : convertThis < 10 && (convertThis = `0${convertThis}`);
+
+  return `${convertThis}:${thisPartIsGood}`;
+}
+
+// maybe less ugly, but still readable
+function timeConversion(s) {
+  let convertThis = parseInt(s.slice(0, 2));
+  const thisPartIsGood = s.slice(3, s.length - 2);
+  if (s.match(/P/g)) {
+    convertThis < 12 && (convertThis += 12);
+  } else if (s.match(/A/g)) {
+    convertThis === 12
+      ? (convertThis = "00")
+      : convertThis < 10 && (convertThis = `0${convertThis}`);
+  }
+  return `${convertThis}:${thisPartIsGood}`;
+}
+
+// kind of ugly
+function timeConversion(s) {
+  let convertThis = parseInt(s.slice(0, 2));
+  const thisPartIsGood = s.slice(3, s.length - 2);
   if (s.match(/P/g)) {
     if (convertThis < 12) {
-      convertThis = convertThis + 12
+      convertThis += 12;
     }
   } else if (s.match(/A/g)) {
     if (convertThis === 12) {
       convertThis = "00";
     } else if (convertThis < 10) {
-      convertThis = `0${convertThis}`
+      convertThis = `0${convertThis}`;
     }
   }
   return `${convertThis}:${thisPartIsGood}`;
