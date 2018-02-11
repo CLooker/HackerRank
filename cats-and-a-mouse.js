0ad6c4883093ef2f899c4e9d6bb57f812cac549d
Chad Looker <chad.looker@gmail.com>
2018-02-11 10:55:02 -0500

    Update cats-and-a-mouse.js

--- cats-and-a-mouse.js
+++ cats-and-a-mouse.js
@@ -1,21 +1,6 @@
 // https://www.hackerrank.com/challenges/cats-and-a-mouse/problem
 
-// imperative
-function catAndMouse(x, y, z) {
-  const mouseADistance = Math.abs(z - x);
-  const mouseBDistance = Math.abs(z - y);
-  let answer = [];
-  if (mouseADistance < mouseBDistance) {
-    answer = answer.concat('Cat A');
-  } else if (mouseADistance > mouseBDistance) {
-    answer = answer.concat('Cat B');
-  } else {
-    answer = answer.concat('Mouse C');
-  }
-  return answer;
-}
-
-// fancy
+// declarative, immutable
 const catAndMouse = (x, y, z) =>
   [null].reduce(
     ({ catX, catY, answer }, _, index) =>
@@ -26,3 +11,18 @@ const catAndMouse = (x, y, z) =>
           : { catX, catY, answer: answer.concat("Mouse C") },
     { catX: Math.abs(z - x), catY: Math.abs(z - y), answer: [] }
   ).answer;
+
+// imperative, mutable
+function catAndMouse(x, y, z) {
+  const mouseADistance = Math.abs(z - x);
+  const mouseBDistance = Math.abs(z - y);
+  let answer = [];
+  if (mouseADistance < mouseBDistance) {
+    answer.push("Cat A");
+  } else if (mouseADistance > mouseBDistance) {
+    answer.push("Cat B");
+  } else {
+    answer.push("Mouse C");
+  }
+  return answer;
+}
