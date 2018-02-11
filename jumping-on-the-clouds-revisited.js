cda1d9189188127d2be4e5255d8ed2fb70a85f58
Chad Looker <chad.looker@gmail.com>
2018-02-11 11:02:20 -0500

    Update jumping-on-the-clouds-revisited.js

--- jumping-on-the-clouds-revisited.js
+++ jumping-on-the-clouds-revisited.js
@@ -1,11 +1,26 @@
 // https://www.hackerrank.com/challenges/jumping-on-the-clouds-revisited/problem
 
-const jumpingOnClouds = (c, k) => (
-    c.reduce(({ energy, ind }, cloud, idx) => (
-        idx === ind
-            ? cloud === 0
-              ? {energy: energy - 1, ind: ind + k}
-              : {energy: energy - 3, ind: ind + k}                
-            : {energy, ind}
-    ), {energy: 100, ind: 0}).energy
-)
+// declarative, immutable, implicit return
+const jumpingOnClouds = (c, k) =>
+  c.reduce(
+    ({ energy, ind }, cloud, idx) =>
+      idx === ind
+        ? cloud === 0
+          ? { energy: --energy, ind: ind + k }
+          : { energy: energy - 3, ind: ind + k }
+        : { energy, ind },
+    { energy: 100, ind: 0 }
+  ).energy;
+
+// imperative, mutable, expicit return
+function jumpingOnClouds(c, k) {
+  let energy = 100;
+  for (let i = 0; i < c.length; i += k) {
+    if (c[i] === 0) {
+      --energy;
+    } else {
+      energy = energy - 3;
+    }
+  }
+  return energy;
+}
