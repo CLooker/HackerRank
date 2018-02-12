caa0f470691f2782b9a31dd21ce90dc35acee57c
Chad Looker <chad.looker@gmail.com>
2018-02-12 07:30:57 -0500

    Update a-very-big-sum.js

--- a-very-big-sum.js
+++ a-very-big-sum.js
@@ -1,5 +1,14 @@
 // https://www.hackerrank.com/challenges/a-very-big-sum/problem
 
-const aVeryBigSum = (n, ar) => (
-  ar.reduce((total, number) => total += number)
-)
+// declarative, immutable, implicit return
+const aVeryBigSum = (n, ar) => ar.reduce((total, number) => total + number);
+
+// imperative, mutable, explicit return
+function aVeryBigSum(n, ar) {
+  let total = 0;
+  for (let i = 0; i < ar.length; i++) {
+    total += ar[i];
+  }
+  return total;
+}
+
