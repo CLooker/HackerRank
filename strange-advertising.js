7b469b6cdbfad94465fbb6e21fc174e7f9716820
Chad Looker <chad.looker@gmail.com>
2018-02-21 16:07:47 +0000

    Update strange-advertising.js

--- strange-advertising.js
+++ strange-advertising.js
@@ -1,12 +1,15 @@
-// https://www.hackerrank.com/challenges/strange-advertising/problem
+// https://www.hackerrank.com/challenges/strange-advertising/problem
 
-const viralAdvertising = n => (
-  Array.from(Array(n))
-    .reduce((days, _, i) => days.concat(i + 1), [])
-    .reduce((dailyLikeTotal, day) => (
-      day === 1
-        ? dailyLikeTotal.concat(2)
-        : dailyLikeTotal.concat(Math.floor((dailyLikeTotal[dailyLikeTotal.length - 1] * 3) / 2))
-    ), [])
-    .reduce((totalLikes, dailyLikes) => totalLikes + dailyLikes) 
-)
+const viralAdvertising = n =>
+  Array.from(Array(n))
+    .reduce((days, _, i) => days.concat(++i), [])
+    .reduce(
+      (dailyLikeTotal, day) =>
+        day === 1
+          ? dailyLikeTotal.concat(2)
+          : dailyLikeTotal.concat(
+              Math.floor(dailyLikeTotal[dailyLikeTotal.length - 1] * 3 / 2)
+            ),
+      []
+    )
+    .reduce((totalLikes, dailyLikes) => totalLikes + dailyLikes);
