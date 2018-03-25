1c613d6ce9735a7312e670bc66cc76076276aada
Chad Looker <chad.looker@gmail.com>
2018-03-25 11:35:24 -0400

    Update camelcase.js

--- camelcase.js
+++ camelcase.js
@@ -1,16 +1,17 @@
 // https://www.hackerrank.com/challenges/camelcase/problem
 
-function camelcase(s) {    
-  let indices = s.split("")
-    .map((l, i) => l === l.toUpperCase() && i)
-    .filter(item => item !== false);    
+function camelcase(s) {
+  let indices = s
+    .split("")
+    .reduce(
+      (indices, l, i) => (l === l.toUpperCase() ? [...indices, i] : indices),
+      [0]
+    );
   
-  indices = [ 0, ...indices ]    
-  
-  let wordsColl = [];    
-  indices.forEach((idx, i) => {         
-    wordsColl.push(s.substring(idx, indices[i + 1]))    
-  });    
+  let wordsColl = [];
+  indices.forEach((idx, i) => {
+    wordsColl.push(s.substring(idx, indices[i + 1]));
+  });
   
   return wordsColl.length;
 }
