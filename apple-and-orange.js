2d082ba09e23693ce05dec2c12758bcd14d4f18d
CLooker <chad.looker@gmail.com>
2018-07-11 12:59:41 -0400

    add solution that uses partially applied curried fn

--- apple-and-orange.js
+++ apple-and-orange.js
@@ -1,5 +1,47 @@
 // https://www.hackerrank.com/challenges/apple-and-orange/problem
 
+//
+const countApplesAndOranges = (
+  startOfHouse,
+  endOfHouse,
+  appleTreeCoord,
+  orangeTreeCoord,
+  distancesOfFallenApples,
+  distancesOfFallenOranges
+) => {
+  const fruitFellOnAHouse = houseCoords => treeCord => fruitDistFromTree => {
+    const fruitCoord = fruitDistFromTree + treeCord;
+    const [houseStart, houseEnd] = houseCoords;
+    const fellPastStart = fruitCoord >= houseStart;
+    const fellBeforeEnd = fruitCoord <= houseEnd;
+    return fellPastStart && fellBeforeEnd;
+  };
+
+  const houseCoords = [startOfHouse, endOfHouse];
+
+  const fruitFellOnThisHouse = fruitFellOnAHouse(houseCoords);
+
+  const applesThatHitThisHouse = (() => {
+    const appleFellOnThisHouse = fruitFellOnThisHouse(appleTreeCoord);
+    const { length: total } = distancesOfFallenApples.filter(
+      appleFellOnThisHouse
+    );
+    return total;
+  })();
+
+  const orangesThatHitThisHouse = (() => {
+    const orangeFellOnThisHouse = fruitFellOnThisHouse(orangeTreeCoord);
+    const { length: total } = distancesOfFallenOranges.filter(
+      orangeFellOnThisHouse
+    );
+    return total;
+  })();
+
+  [applesThatHitThisHouse, orangesThatHitThisHouse].forEach(total =>
+    console.log(total)
+  );
+};
+
 // declarative, immutable, side-effects pushed to edge
 const countApplesAndOranges = (s, t, a, b, apple, orange) =>
   [
