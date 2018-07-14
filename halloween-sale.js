128db1224a5c19d398087ac66eeb683e855abe72
CLooker <chad.looker@gmail.com>
2018-07-14 00:34:27 -0400

    add solution for halloween-sale

---
+++ halloween-sale.js
@@ -0,0 +1,26 @@
+// https://www.hackerrank.com/challenges/halloween-sale/problem
+
+function howManyGames(price, subtractFactor, priceFloor, totalMoney) {
+  let totalGames = 0;
+  const hasEnoughMoney = p => totalMoney >= p;
+
+  if (!hasEnoughMoney(price)) {
+    return totalGames;
+  }
+
+  while (price > priceFloor) {
+    if (!hasEnoughMoney(price)) {
+      return totalGames;
+    }
+    totalGames++;
+    totalMoney -= price;
+    price -= subtractFactor;
+  }
+
+  while (hasEnoughMoney(priceFloor)) {
+    totalGames++;
+    totalMoney -= priceFloor;
+  }
+
+  return totalGames;
+}
