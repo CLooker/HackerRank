c08ba11f7b6db109dec35be2c4dd22855774699b
CLooker <chad.looker@gmail.com>
2018-07-14 01:09:43 -0400

    add solution

--- camelcase.js
+++ camelcase.js
@@ -1,9 +1,47 @@
 // https://www.hackerrank.com/challenges/camelcase/problem
 // This exercise times out when using immutable data structures
 
+const camelcase = s => {
+  const firstLetterIndexes = (() => {
+    const arr = [];
+    for (let i = 0; i < s.length; i++) {
+      const letter = s[i];
+      const isCapitalized = letter.toUpperCase() === letter;
+      if (isCapitalized) {
+        arr.push(i);
+      }
+    }
+    return arr;
+  })();
+
+  const isOnlyOneWord = firstLetterIndexes.length === 0;
+
+  if (isOnlyOneWord) {
+    return 1;
+  }
+
+  const words = (() => {
+    const arr = [];
+    firstLetterIndexes.forEach((idx, i) => {
+      const wordsIsEmpty = arr.length === 0;
+      if (wordsIsEmpty) {
+        const firstWord = s.slice(0, idx);
+        arr.push(firstWord);
+      }
+      const word = s.slice(idx, firstLetterIndexes[i + 1]);
+      arr.push(word);
+    });
+    return arr;
+  })();
+
+  return words.length;
+};
+
+///////////////////////////////////
+
 const getIndicesColl = coll =>
   coll
-    .split("")
+    .split('')
     .reduce(
       (indices, l, i) =>
         l === l.toUpperCase() ? indices.push(i) && indices : indices,
