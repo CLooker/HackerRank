9dd5c95a8c9ce5eebd10a32870b6e64e78e50123
CLooker <chad.looker@gmail.com>
2018-07-15 18:50:34 -0400

    add solution to reduced-string

---
+++ reduced-string.js
@@ -0,0 +1,24 @@
+function super_reduced_string(s) {
+  while (true) {
+    let deleted = false;
+
+    for (let i = 0; i < s.length; i++) {
+      const letter = s[i];
+      const nextLetter = s[i + 1];
+      const shouldDelete = letter === nextLetter;
+      if (shouldDelete) {
+        deleted = true;
+        const start = s.slice(0, i);
+        const end = s.slice(i + 2);
+        s = `${start}${end}`;
+        break;
+      }
+    }
+
+    if (!deleted) {
+      break;
+    }
+  }
+
+  return s ? s : 'Empty String';
+}
