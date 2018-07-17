3f8c1b45ba1497f9bb4022678b92148cadfe9708
CLooker <chad.looker@gmail.com>
2018-07-17 08:32:27 -0400

    add happy-ladybugs solution

---
+++ happy-ladybugs.js
@@ -0,0 +1,20 @@
+// https://www.hackerrank.com/challenges/happy-ladybugs/problem
+
+function happyLadybugs(b) {
+  const bArr = [...b];
+  const isColor = x => x !== '_';
+
+  for (let [i, char] of bArr.entries()) {
+    if (!isColor(char)) continue;
+    const atLeastOneMatch = bArr.some((otherChar, j) => {
+      if (i === j || char !== otherChar) return false;
+      const matchesAreAdjacent = Math.abs(i - j) === 1;
+      if (matchesAreAdjacent) return true;
+      const atLeastOneEmptySpace = bArr.some(item => !isColor(item));
+      if (atLeastOneEmptySpace) return true;
+    });
+    if (!atLeastOneMatch) return 'NO';
+  }
+
+  return 'YES';
+}
