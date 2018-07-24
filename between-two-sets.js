12df8b0b842e974144ea096bded2b7f74acd41c3
CLooker <chad.looker@gmail.com>
2018-07-24 17:00:13 -0400

    extract into one function

--- between-two-sets.js
+++ between-two-sets.js
@@ -1,54 +1,46 @@
 // https://www.hackerrank.com/challenges/between-two-sets/problem
 
 const getTotalX = (a, b) => {
-  // every element of a
-  // divides evenly into every element of aFactors
-  const aFactors = (() => {
+  const getAFactorsOrXs = ({ nums, otherNums, getAFactors = false }) => {
     let arr = [];
-    const firstBNum = b[0];
 
-    for (
-      let possibleFactor = 1;
-      possibleFactor <= firstBNum;
-      possibleFactor++
-    ) {
-      let isFactor = true;
+    for (const num of nums) {
+      let isValidForAll = true;
 
-      for (const aNum of a) {
-        const isNotFactor = possibleFactor % aNum !== 0;
-        if (isNotFactor) {
-          isFactor = false;
+      for (const otherNum of otherNums) {
+        const isValid = getAFactors
+          ? num % otherNum === 0
+          : otherNum % num === 0;
+        if (!isValid) {
+          isValidForAll = false;
           break;
         }
       }
 
-      if (isFactor) arr.push(possibleFactor);
+      if (isValidForAll) arr.push(num);
     }
 
     return arr;
-  })();
+  };
 
-  // every element of a
-  // divides evenly into every element of allXs
-  // every element of allXs
-  // divides evenly into every element of b
-  const allXs = (() => {
-    let arr = [];
+  const lastPossibleAFactor = b[0];
+  const possibleAFactors = Array.from(Array(lastPossibleAFactor)).map(
+    (_, i) => ++i
+  );
 
-    for (const aFactor of aFactors) {
-      let isX = true;
-      for (const bNum of b) {
-        const isNotX = bNum % aFactor !== 0;
-        if (isNotX) {
-          isX = false;
-          break;
-        }
-      }
-      if (isX) arr.push(aFactor);
-    }
+  // every element of `a`
+  // divides evenly into every element of `aFactors`
+  const aFactors = getAFactorsOrXs({
+    nums: possibleAFactors,
+    otherNums: a,
+    getAFactors: true
+  });
 
-    return arr;
-  })();
+  // every element of `a`
+  // divides evenly into every element of `allXs`
+  // every element of `allXs`
+  // divides evenly into every element of `b`
+  const allXs = getAFactorsOrXs({ nums: aFactors, otherNums: b });
 
   return allXs.length;
 };
