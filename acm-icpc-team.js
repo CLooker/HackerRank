963152a92edfea78925016844def6ef34fd32cf5
CLooker <chad.looker@gmail.com>
2018-07-24 12:40:39 -0400

    make 1-indexing obvious

--- acm-icpc-team.js
+++ acm-icpc-team.js
@@ -1,9 +1,10 @@
 // https://www.hackerrank.com/challenges/acm-icpc-team/problem
 
 function acmTeam(attendees) {
-  // returns obj mapping from attendees
-  // to other attendees for which best combinations
+  // returns 1-indexed obj mapping from attendees
+  // to array of other attendees for which combinations
   // should be computed
+  // prevents computing more than once
   const validAttendeeCombos = (() => {
     const dict = {};
     const { length } = attendees;
@@ -20,7 +21,7 @@ function acmTeam(attendees) {
     return dict;
   })();
 
-  // should we compute combination of scores
+  // should we compute combination
   const isValidCombo = (attendee, otherAttendee) => {
     const isAttendeeValid = validAttendeeCombos[attendee];
     if (!isAttendeeValid) return false;
@@ -36,23 +37,20 @@ function acmTeam(attendees) {
 
   const returnTotal = coll => coll.reduce((total, num) => total + num, 0);
 
-  // start off with 0 as the most topics known
   let mostTopicsKnownColl = [0];
 
   for (let [i, attendee] of attendees.entries()) {
     // validAttedeeCombos is 1-indexed
-    i += 1;
-
-    if (!isValidCombo(i)) break;
+    const attendeeIdx = i + 1;
+    if (!isValidCombo(attendeeIdx)) break;
 
     // best score combo between this attendee and all others
-    // TODO RENAME
     let bestComboForThisAttendee = [];
 
     for (let [j, otherAttendee] of attendees.entries()) {
       // validAttedeeCombos is 1-indexed
-      j += 1;
-      if (!isValidCombo(i, j)) continue;
+      const otherAttendeeIdx = j + 1;
+      if (!isValidCombo(attendeeIdx, otherAttendeeIdx)) continue;
 
       // if we have all 1's, we don't need new combos
       const isBestPossibleCombo =
@@ -76,20 +74,18 @@ function acmTeam(attendees) {
         combo.push(0);
       }
 
+      // compare this to the best combo attendee has so far
       const topicsKnown = returnTotal(combo);
       const attendeeMostTopicsKnown = returnTotal(bestComboForThisAttendee);
       const isNewBest = topicsKnown > attendeeMostTopicsKnown;
-
-      // compare this to the best combo attendee has so far
       if (isNewBest) bestComboForThisAttendee = combo;
     }
 
+    // compare this attendee's best versus the all-time best
     const attendeeMostTopicsKnown = returnTotal(bestComboForThisAttendee);
     const [mostTopicsKnown] = mostTopicsKnownColl;
     const isMatchingMostKnownTopics =
       attendeeMostTopicsKnown === mostTopicsKnown;
-
-    // compare this attendee's best versus the all-time best
     if (isMatchingMostKnownTopics) {
       mostTopicsKnownColl.push(attendeeMostTopicsKnown);
       continue;
