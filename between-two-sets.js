49c3230bb828c6a4909ece44c57330c68c2d4491
CLooker <chad.looker@gmail.com>
2018-07-26 18:59:35 -0400

    add comments

--- between-two-sets.js
+++ between-two-sets.js
@@ -1,5 +1,7 @@
 // https://www.hackerrank.com/challenges/between-two-sets/problem
 
+// every member of `denominators` evenly divides every member of `possibleXs`
+// (denominators: Number[], lastPossibleX: Number) -> Number[]
 const getPossibleXs = (denominators, lastPossibleX) => {
   const firstPossibleX = denominators[denominators.length - 1];
   let possibleXs = [];
@@ -22,6 +24,8 @@ const getPossibleXs = (denominators, lastPossibleX) => {
   return possibleXs;
 };
 
+// every member of returned array evenly divides every member of `numerators`
+// (numerators: Number[], possibleXs: Number[]) -> Number[]
 const getXs = (numerators, possibleXs) =>
   possibleXs.filter(possibleX => {
     const isXForAllNumerators = numerators.every(numerator => {
@@ -32,6 +36,10 @@ const getXs = (numerators, possibleXs) =>
     return isXForAllNumerators;
   });
 
+// every member of `a` evenly divides every member of `xS`
+// every member of `xS` evenly divides every member of `b`
+// returns the length of `xS`
+// (a: Number[], b: Number[]) -> Number
 const getTotalX = (a, b) => {
   const [lastPossibleX] = b;
   const possibleXs = getPossibleXs(a, lastPossibleX);
