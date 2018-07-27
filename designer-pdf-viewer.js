9552a79d780860ef859c770f411e8c2285492d46
CLooker <chad.looker@gmail.com>
2018-07-27 00:19:13 -0400

    add readable solution

--- designer-pdf-viewer.js
+++ designer-pdf-viewer.js
@@ -1,31 +1,30 @@
 // https://www.hackerrank.com/challenges/designer-pdf-viewer/problem
 
+function designerPdfViewer(h, word) {
+  const alphabet = [...'abcdefghijklmnopqrstuvwxyz'];
+
+  const abcIdxes = alphabet.reduce((idxes, letter, i) => {
+    const isLetterIncluded = word.includes(letter);
+    return isLetterIncluded ? idxes.concat(i) : idxes;
+  }, []);
+
+  const hIdxes = abcIdxes.map(abcIdx => {
+    const hIdx = h[abcIdx];
+    return hIdx;
+  });
+
+  const hIdxesSorted = [...hIdxes].sort((a, b) => b - a);
+  const [height] = hIdxesSorted;
+  const width = word.length;
+  return height * width;
+}
+
 // declarative, immutable, implicit return
 const designerPdfViewer = (h, word) => (
-  (height = "abcdefghijklmnopqrstuvwxyz"
-    .split("")
+  (height = 'abcdefghijklmnopqrstuvwxyz'
+    .split('')
     .map((item, index) => word.includes(item) && index)
     .map(idx => h[idx])
     .sort((a, b) => b - a)[0]),
   height * word.length
 );
-
-// imperative, mutable, explicit return
-function designerPdfViewer(h, word) {
-  const alphabet = "abcdefghijklmnopqrstuvwxyz";
-  let abcIdxes = [];
-  for (let i = 0; i < alphabet.length; i++) {
-    if (word.includes(alphabet[i])) {
-      abcIdxes.push(i);
-    }
-  }
-  let mapAbcIdxesToHIdxes = [];
-  for (let i = 0; i < abcIdxes.length; i++) {
-    let target = abcIdxes[i];
-    mapAbcIdxesToHIdxes.push(h[target]);
-  }
-  let hIdxesSorted = mapAbcIdxesToHIdxes.sort((a, b) => b - a);
-  let height = hIdxesSorted[0];
-  let width = word.length;
-  return height * width;
-}
