eeabfa4e9fc0456d0d5e7d74bc3fe875c0ce42a1
CLooker <chad.looker@gmail.com>
2018-07-27 13:31:36 -0400

    add readable solution

--- divisible-sum-pairs.js
+++ divisible-sum-pairs.js
@@ -1,11 +1,16 @@
 // https://www.hackerrank.com/challenges/divisible-sum-pairs/problem
 
-const divisibleSumPairs = (n, k, ar) => (
-  ar.reduce((result, item, index) => {
-    let tempSumList = []
-    for (let i = index + 1; i < ar.length; i++) {
-      ((item + ar[i]) % k === 0) && (tempSumList = [...tempSumList, [index, i]]);
-    }
-    return result += tempSumList.length;
-  }, 0)
-)
+const divisibleSumPairs = (_, divisor, nums) => {
+  let total = 0;
+
+  nums.forEach((num, i) => {
+    const nextNums = nums.slice(i + 1);
+    nextNums.forEach(nextNum => {
+      const sumPair = num + nextNum;
+      const isDivisibleSumPair = sumPair % divisor === 0;
+      if (isDivisibleSumPair) total++;
+    });
+  });
+
+  return total;
+};
