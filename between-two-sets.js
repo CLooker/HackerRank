a8630630f55cb67b46a6d38a40b690a4417fabd6
CLooker <chad.looker@gmail.com>
2018-07-26 21:41:04 -0400

    add best solution

--- between-two-sets.js
+++ between-two-sets.js
@@ -1,48 +1,34 @@
 // https://www.hackerrank.com/challenges/between-two-sets/problem
 
-// every member of `denominators` evenly divides every member of `possibleXs`
-// (denominators: Number[], lastPossibleX: Number) -> Number[]
-const getPossibleXs = (denominators, lastPossibleX) => {
-  const firstPossibleX = denominators[denominators.length - 1];
-  let possibleXs = [];
+// (leftNums: Number[], rightNums: Number[]) -> Number[]
+const getTotalX = (leftNums, rightNums) => {
+  const middleNums = (() => {
+    const firstPossibleMiddleNum = leftNums[leftNums.length - 1];
+    const [lastPossibleMiddleNum] = rightNums;
 
-  for (
-    let possibleX = firstPossibleX;
-    possibleX <= lastPossibleX;
-    possibleX += firstPossibleX
-  ) {
-    const isEvenlyDivisibleByAllDenominators = denominators.every(
-      denominator => {
-        const isEvenlyDisible = possibleX % denominator === 0;
-        return isEvenlyDisible;
-      }
-    );
+    let middleNums = [];
 
-    if (isEvenlyDivisibleByAllDenominators) possibleXs.push(possibleX);
-  }
+    for (
+      let num = firstPossibleMiddleNum;
+      num <= lastPossibleMiddleNum;
+      num += firstPossibleMiddleNum
+    ) {
+      const isNumEvenlyDivisbleByAllLeftNums = leftNums.every(
+        leftNum => num % leftNum === 0
+      );
 
-  return possibleXs;
-};
+      const isNumAFactorForAllRightNums = rightNums.every(
+        rightNum => rightNum % num === 0
+      );
 
-// every member of returned array evenly divides every member of `numerators`
-// (numerators: Number[], possibleXs: Number[]) -> Number[]
-const getXs = (numerators, possibleXs) =>
-  possibleXs.filter(possibleX => {
-    const isXForAllNumerators = numerators.every(numerator => {
-      const isX = numerator % possibleX === 0;
-      return isX;
-    });
+      const isMiddleNum =
+        isNumEvenlyDivisbleByAllLeftNums && isNumAFactorForAllRightNums;
 
-    return isXForAllNumerators;
-  });
+      if (isMiddleNum) middleNums = middleNums.concat(num);
+    }
 
-// every member of `a` evenly divides every member of `xS`
-// every member of `xS` evenly divides every member of `b`
-// returns the length of `xS`
-// (a: Number[], b: Number[]) -> Number
-const getTotalX = (a, b) => {
-  const [lastPossibleX] = b;
-  const possibleXs = getPossibleXs(a, lastPossibleX);
-  const xS = getXs(b, possibleXs);
-  return xS.length;
+    return middleNums;
+  })();
+
+  return middleNums.length;
 };
