e4f3244f575314f71db4b052fbcbbeadd8a9148e
CLooker <chad.looker@gmail.com>
2018-07-26 22:14:39 -0400

    use for of

--- a-very-big-sum.js
+++ a-very-big-sum.js
@@ -1,14 +1,13 @@
 // https://www.hackerrank.com/challenges/a-very-big-sum/problem
 
 // declarative, immutable, implicit return
-const aVeryBigSum = (n, ar) => ar.reduce((total, number) => total + number);
+const aVeryBigSum = ar => ar.reduce((total, number) => total + number);
 
 // imperative, mutable, explicit return
-function aVeryBigSum(n, ar) {
+function aVeryBigSum(ar) {
   let total = 0;
-  for (let i = 0; i < ar.length; i++) {
-    total += ar[i];
+  for (let num of ar) {
+    total += num;
   }
   return total;
 }
-
