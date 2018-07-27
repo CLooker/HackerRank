dd74c8eddf1752f22540d8b9451049e5a41902a1
CLooker <chad.looker@gmail.com>
2018-07-27 12:01:02 -0400

    replace with readable solution

--- diagonal-difference.js
+++ diagonal-difference.js
@@ -1,58 +1,21 @@
-// https://www.hackerrank.com/challenges/diagonal-difference/problem
+const diagonalDifference = matrix => {
+  const primaryDiagonal = matrix.map((row, i) => {
+    const primaryDiagonolCharacter = row[i];
+    return primaryDiagonolCharacter;
+  });
 
+  const secondaryDiagonal = (() => {
+    const reversedMatrix = [...matrix].reverse();
+    return reversedMatrix.map((row, i) => {
+      const secondaryDiagonalCharacter = row[i];
+      return secondaryDiagonalCharacter;
+    });
+  })();
 
-// declarative, immutable, fn composition, implicit return
-const primaryDiagonol = arr =>
-  arr.map((numberArray, index) => numberArray[index]);
-const secondaryDiagonol = arr =>
-  arr.reverse().map((numberArray, index) => numberArray[index]);
-const primaryDiagonolSum = primaryDiagonolArr =>
-  primaryDiagonolArr.reduce((total, number) => (total += number));
-const secondaryDiagonolSum = secondaryDiagonolArr =>
-  secondaryDiagonolArr.reduce((total, number) => (total += number));
+  const getSum = arr => arr.reduce((total, num) => total + num, 0);
 
-const diagonalDifference = a =>
-  Math.abs(
-    primaryDiagonolSum(primaryDiagonol(a)) -
-    secondaryDiagonolSum(secondaryDiagonol(a))
-  );
+  const primarySum = getSum(primaryDiagonal);
+  const secondarySum = getSum(secondaryDiagonal);
 
-
-// declarative, immutable
-function diagonalDifference(a) {
-  const primaryDiagonol = a.map((numberArray, index) => numberArray[index]);
-  const secondaryDiagonol = a
-    .reverse()
-    .map((numberArray, index) => numberArray[index]);
-  const primaryDiagonolSum = primaryDiagonol.reduce(
-    (total, number) => (total += number)
-  );
-  const secondaryDiagonolSum = secondaryDiagonol.reduce(
-    (total, number) => (total += number)
-  );
-  return Math.abs(primaryDiagonolSum - secondaryDiagonolSum);
-}
-
-// imperative, mutable
-function diagonalDifference(a) {
-  let primaryDiagonol = [];
-  for (let i = 0; i < a.length; i++) {
-    let numberArray = a[i];
-    primaryDiagonol.push(numberArray[i]);
-  }
-  let secondaryDiagonol = [];
-  let reversedA = a.reverse();
-  for (let i = 0; i < a.length; i++) {
-    let numberArray = a[i];
-    secondaryDiagonol.push(numberArray[i]);
-  }
-  let primaryDiagonolSum = 0;
-  for (let i = 0; i < primaryDiagonol.length; i++) {
-    primaryDiagonolSum += primaryDiagonol[i];
-  }
-  let secondaryDiagonolSum = 0;
-  for (let i = 0; i < primaryDiagonol.length; i++) {
-    secondaryDiagonolSum += secondaryDiagonol[i];
-  }
-  return Math.abs(primaryDiagonolSum - secondaryDiagonolSum);
-}
+  return Math.abs(primarySum - secondarySum);
+};
