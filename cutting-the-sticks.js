6744faf7303f23a05b807efc21892d4f7b7d3104
CLooker <chad.looker@gmail.com>
2018-07-27 00:07:29 -0400

    add readable solution

--- cutting-the-sticks.js
+++ cutting-the-sticks.js
@@ -1,113 +1,43 @@
 // https://www.hackerrank.com/challenges/cut-the-sticks/problem
 
-// declarative, immutable, recursive, ES6 arrow IIFE, implicit return
-const cutTheSticks = arr =>
-  (handleSticks = ({ sticks, sticksLeftPerTurn, findShortestStick }) => (
-    (shortestStick = findShortestStick(sticks)),
-    sticks.length > 0
-      ? handleSticks({
-          sticks: sticks
-            .map(stick => stick - shortestStick)
-            .filter(stick => stick > 0),
-          sticksLeftPerTurn: [...sticksLeftPerTurn, sticks.length],
-          findShortestStick
-        })
-      : sticksLeftPerTurn
-  ))({
-    sticks: [...arr],
-    sticksLeftPerTurn: [],
-    findShortestStick: stickArr => stickArr.slice(0).sort((a, b) => a - b)[0]
-  });
+const cutTheSticks = sticks => {
+  // don't mutate param
+  sticks = [...sticks].sort((a, b) => a - b);
 
-// declarative, immutable, recursive, ES6 arrow IIFE
-const cutTheSticks = arr =>
-  (handleSticks = ({ sticks, sticksLeftPerTurn, findShortestStick }) => {
-    const shortestStick = findShortestStick(sticks);
-    return sticks.length > 0
-      ? handleSticks({
-          sticks: sticks
-            .map(stick => stick - shortestStick)
-            .filter(stick => stick > 0),
-          sticksLeftPerTurn: [...sticksLeftPerTurn, sticks.length],
-          findShortestStick
-        })
-      : sticksLeftPerTurn;
-  })({
-    sticks: [...arr],
-    sticksLeftPerTurn: [],
-    findShortestStick: stickArr => stickArr.slice(0).sort((a, b) => a - b)[0]
-  });
+  let sticksLeftPerTurn = [];
+  while (sticks.length) {
+    const { length } = sticks;
+    sticksLeftPerTurn.push(length);
 
-// declarative, immutable, recursive, IIFE
-const cutTheSticks = arr =>
-  (function handleSticks({ sticks, sticksLeftPerTurn, findShortestStick }) {
-    const shortestStick = findShortestStick(sticks);
-    return sticks.length > 0
-      ? handleSticks({
-          sticks: sticks
-            .map(stick => stick - shortestStick)
-            .filter(stick => stick > 0),
-          sticksLeftPerTurn: [...sticksLeftPerTurn, sticks.length],
-          findShortestStick
-        })
-      : sticksLeftPerTurn;
-  })({
-    sticks: [...arr],
-    sticksLeftPerTurn: [],
-    findShortestStick: stickArr => stickArr.slice(0).sort((a, b) => a - b)[0]
-  });
+    const [shortestStick] = sticks;
+    const sticksGreaterThanZero = sticks.filter(stick => {
+      const newStick = stick - shortestStick;
+      const isGreaterThanZero = newStick > 0;
+      return isGreaterThanZero;
+    });
 
-// // declarative, immutable, recursive, function extraction
-const handleSticks = ({ sticks, sticksLeftPerTurn, findShortestStick }) => {
-  const shortestStick = findShortestStick(sticks);
-  return sticks.length > 0
-    ? handleSticks({
-        sticks: sticks
-          .map(stick => stick - shortestStick)
-          .filter(stick => stick > 0),
-        sticksLeftPerTurn: [...sticksLeftPerTurn, sticks.length],
-        findShortestStick
-      })
-    : sticksLeftPerTurn;
+    sticks = sticksGreaterThanZero;
+  }
+  return sticksLeftPerTurn;
 };
+
+// declarative, immutable, recursive, function extraction
+const handleSticks = ({ sticks, sticksLeftPerTurn, shortestStick }) => {
+  if (!sticks.length) return sticksLeftPerTurn;
+
+  return handleSticks({
+    sticks: sticks.filter(stick => {
+      const [shortestStick] = sticks;
+      const newStick = stick - shortestStick;
+      const isGreaterThanZero = newStick > 0;
+      return isGreaterThanZero;
+    }),
+    sticksLeftPerTurn: [...sticksLeftPerTurn, sticks.length]
+  });
+};
+
 const cutTheSticks = arr =>
   handleSticks({
-    sticks: [...arr],
-    sticksLeftPerTurn: [],
-    findShortestStick: stickArr => stickArr.slice(0).sort((a, b) => a - b)[0]
+    sticks: [...arr].sort((a, b) => a - b),
+    sticksLeftPerTurn: []
   });
-
-// imperative, immutable
-const cutTheSticks = arr => {
-  let sticks = [...arr];
-  let sticksLeftPerTurn = [];
-  while (sticks.length > 0) {
-    sticksLeftPerTurn = [...sticksLeftPerTurn, sticks.length];
-    const shortestStick = sticks.sort((a, b) => a - b)[0];
-    sticks = sticks
-      .map(stick => stick - shortestStick)
-      .filter(stick => stick > 0);
-  }
-  return sticksLeftPerTurn;
-};
-
-// imperative, mutable
-function cutTheSticks(arr) {
-  let sticksLeftPerTurn = [];
-  while (arr.length > 0) {
-    sticksLeftPerTurn.push(arr.length);
-    const shortestStick = arr.sort((a, b) => a - b)[0];
-    let sticks = [];
-    for (let i = 0; i < arr.length; i++) {
-      sticks.push(arr[i] - shortestStick);
-    }
-    let sticksGreaterThanZero = [];
-    for (let i = 0; i < sticks.length; i++) {
-      if (sticks[i] > 0) {
-        sticksGreaterThanZero.push(sticks[i]);
-      }
-    }
-    arr = sticksGreaterThanZero;
-  }
-  return sticksLeftPerTurn;
-}
