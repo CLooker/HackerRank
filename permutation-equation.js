57b7ab26d2f8030695f37b9ff1bcd3e504f7c2bb
CLooker <chad.looker@gmail.com>
2018-07-28 00:55:50 -0400

    make changes for readability

--- permutation-equation.js
+++ permutation-equation.js
@@ -1,17 +1,8 @@
 // https://www.hackerrank.com/challenges/permutation-equation/problem
 
-function permutationEquation(p) {
-  const sorted = [...p].sort((a, b) => a - b);
-
-  const permutated = (() => {
-    let arr = [];
-    sorted.forEach(val => {
-      const nextVal = p.indexOf(val) + 1;
-      const finalVal = p.indexOf(nextVal) + 1;
-      arr.push(finalVal);
-    });
-    return arr;
-  })();
-
-  return permutated;
-}
+const permutationEquation = nums =>
+  [...nums].sort((a, b) => a - b).map(num => {
+    const nextNumIdx = nums.indexOf(num) + 1;
+    const val = nums.indexOf(nextNumIdx) + 1;
+    return val;
+  });
