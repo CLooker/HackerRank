06dc83da507214dbefd4d2e24af86bbafda5613c
CLooker <chad.looker@gmail.com>
2018-07-27 23:17:17 -0400

    made small changes

--- grading.js
+++ grading.js
@@ -3,21 +3,20 @@
 function nextFiveMultiple(grade) {
   let i = grade;
   do {
-    i++
-  }
-  while (i % 5 !== 0)
+    i++;
+  } while (i % 5 !== 0);
   return i;
 }
 
-const solve = grades => (
+const gradingStudents = grades =>
   grades.map(grade => {
     const nextFiveMultipleReturned = nextFiveMultiple(grade);
-    if (grade < 38) {
-      return grade;
-    } else if ((nextFiveMultipleReturned - grade) < 3) {
-      return nextFiveMultipleReturned;
-    } else if ((nextFiveMultipleReturned - grade) >= 3) {
-      return grade;
+    switch (true) {
+      case grade < 38:
+        return grade;
+      case nextFiveMultipleReturned - grade < 3:
+        return nextFiveMultipleReturned;
+      default:
+        return grade;
     }
-  }).forEach(grade => console.log(grade))
-)
+  });
