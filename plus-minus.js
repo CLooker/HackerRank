69f3ee4eb62ec5a41e026066c7ce9887e203d6ee
CLooker <chad.looker@gmail.com>
2018-07-28 11:47:43 -0400

    make changes for readability

--- plus-minus.js
+++ plus-minus.js
@@ -1,39 +1,23 @@
 // https://www.hackerrank.com/challenges/plus-minus/problem
 
-// declarative, immutable
-const plusMinus = arr => {
-  const plusMinusCount = arr.reduce(
-    ({ posCount, negCount, zeroCount }, n) =>
-      n > 0
-        ? { posCount: ++posCount, negCount, zeroCount }
-        : n < 0
-          ? { posCount, negCount: ++negCount, zeroCount }
-          : { posCount, negCount, zeroCount: ++zeroCount },
-    { posCount: 0, negCount: 0, zeroCount: 0 }
-  );
+const plusMinus = integers => {
+  let plusCounter = 0;
+  let minusCounter = 0;
+  let zeroCounter = 0;
 
-  Object.keys(plusMinusCount).forEach(key =>
-    console.log(plusMinusCount[key] / arr.length)
-  );
+  integers.forEach(integer => {
+    switch (true) {
+      case integer > 0:
+        return plusCounter++;
+      case integer < 0:
+        return minusCounter++;
+      default:
+        return zeroCounter++;
+    }
+  });
+
+  const { length } = integers;
+  [plusCounter, minusCounter, zeroCounter].forEach(counter => {
+    console.log(counter / length);
+  });
 };
-
-// imperative, mutable
-function plusMinus(arr) {
-  let plusCounter = 0;
-  let minusCounter = 0;
-  let zeroCounter = 0;
-  const length = arr.length;
-  for (let i = 0; i < arr.length; i++) {
-    if (arr[i] > 0) {
-      ++plusCounter;
-    } else if (arr[i] < 0) {
-      ++minusCounter;
-    } else {
-      ++zeroCounter;
-    }
-  }
-
-  console.log(
-    `${plusCounter / length}\n${minusCounter / length}\n${zeroCounter / length}`
-  );
-}
