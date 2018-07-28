e7482abba676df5c05c08d894c49713c083c7d3f
CLooker <chad.looker@gmail.com>
2018-07-27 22:58:53 -0400

    add readable solution

--- equality-in-a-array.js
+++ equality-in-a-array.js
@@ -1,45 +1,19 @@
 // https://www.hackerrank.com/challenges/equality-in-a-array/problem
 
-// imperative
-function equalizeArray(arr) {
-  let coll = [];
-  for (let i = 0; i < arr.length; i++) {
-    let tempColl = [];
-    for (let j = 0; j < arr.length; j++) {
-      if (arr[j] === arr[i]) {
-        tempColl.push(arr[j]);
-      }
-      coll.push(tempColl);
-    }
-  }
-  let biggestTempColl = 0;
-  for (let i = 0; i < coll.length; i++) {
-    if (coll[i].length > biggestTempColl) {
-      biggestTempColl = coll[i].length;
-    }
-  }
-  return arr.length - biggestTempColl;
-}
+const equalizeArray = arr => {
+  const highestFreq = (() => {
+    let numberToFreq = {};
 
-// probably not as readable, but declarative, immutable, implicit returns
-const equalizeArray = arr =>
-  arr.length -
-  Array.from(Array(arr.length))
-    .reduce(
-      (coll, _, i) =>
-        Array.from(Array(arr.length)).reduce(
-          (tempColl, _, j) =>
-            arr[j] === arr[i]
-              ? ((tempColl = [...tempColl, arr[j]]),
-                (coll = [...coll, tempColl]),
-                tempColl)
-              : tempColl,
-          []
-        ) && coll,
-      []
-    )
-    .reduce(
-      (biggestTempColl, tempColl, index) =>
-        tempColl.length > biggestTempColl ? tempColl.length : biggestTempColl,
-      0
-    );
+    arr.forEach(num => {
+      const isNumberInDict = numberToFreq[num];
+      isNumberInDict ? numberToFreq[num]++ : (numberToFreq[num] = 1);
+    });
+
+    return Object.values(numberToFreq).reduce((highestFreq, freq) => {
+      const isHighestFreq = freq > highestFreq;
+      return isHighestFreq ? freq : highestFreq;
+    }, 0);
+  })();
+
+  return arr.length - highestFreq;
+};
