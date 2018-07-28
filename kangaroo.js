a2d51635f4e7134908f3f4806c06cafa68f856bd
CLooker <chad.looker@gmail.com>
2018-07-28 00:04:48 -0400

    make changes for readability

--- kangaroo.js
+++ kangaroo.js
@@ -1,6 +1,5 @@
 // https://www.hackerrank.com/challenges/kangaroo/problem
 
-// more code, but probably more quickly understood
 const kangaroo = (xLocation, xJumpSize, yLocation, yJumpSize) => {
   const cannotCatchUp = (() => {
     const xStartsAhead = xLocation > yLocation;
@@ -14,9 +13,7 @@ const kangaroo = (xLocation, xJumpSize, yLocation, yJumpSize) => {
     return xCannotCatchUp || yCannotCatchUp;
   })();
 
-  if (cannotCatchUp) {
-    return 'NO';
-  }
+  if (cannotCatchUp) return 'NO';
 
   const xStartsAhead = xLocation > yLocation;
   const yStartsAhead = yLocation > xLocation;
@@ -36,26 +33,3 @@ const kangaroo = (xLocation, xJumpSize, yLocation, yJumpSize) => {
   const atSameLocation = xLocation === yLocation;
   return atSameLocation ? 'YES' : 'NO';
 };
-
-// least amount of code
-function kangaroo(xLocation, xJumpSize, yLocation, yJumpSize) {
-  if (
-    (xLocation > yLocation && xJumpSize >= yJumpSize) ||
-    (yLocation > xLocation && yJumpSize >= xJumpSize)
-  ) {
-    return 'NO';
-  } else {
-    if (xLocation > yLocation) {
-      while (xLocation > yLocation) {
-        xLocation += xJumpSize;
-        yLocation += yJumpSize;
-      }
-    } else if (yLocation > xLocation) {
-      while (yLocation > xLocation) {
-        xLocation += xJumpSize;
-        yLocation += yJumpSize;
-      }
-    }
-    return xLocation === yLocation ? 'YES' : 'NO';
-  }
-}
