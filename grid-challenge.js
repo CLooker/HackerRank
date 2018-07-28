8a295428633a750ac6974b199a6d09ba3b8ab291
CLooker <chad.looker@gmail.com>
2018-07-27 23:46:20 -0400

    make changes for readability

--- grid-challenge.js
+++ grid-challenge.js
@@ -1,6 +1,5 @@
 // https://www.hackerrank.com/challenges/grid-challenge/problem?h_r=next-challenge&h_v=zen
 
-// mixture that might be easiest to read
 const gridChallenge = grid => {
   const gridWithOrderedRows = grid.map(seq =>
     seq
@@ -11,13 +10,11 @@ const gridChallenge = grid => {
 
   let answer = 'YES';
 
-  gridWithOrderedRows.forEach((row, i) => {
-    row.split('').forEach((letter, j) => {
-      // use for loop because counter is non-standard
-      // for loop is probably easier to read than alternatives
-      for (let k = i + 1; k < gridWithOrderedRows.length; k++) {
-        const otherRow = gridWithOrderedRows[k];
-        const otherLetter = otherRow[j];
+  gridWithOrderedRows.forEach((row, rowIdx) => {
+    row.split('').forEach((letter, letterIdx) => {
+      const otherRows = gridWithOrderedRows.slice(rowIdx + 1);
+      for (const otherRow of otherRows) {
+        const otherLetter = otherRow[letterIdx];
         if (otherLetter < letter) {
           answer = 'NO';
           break;
@@ -27,69 +24,4 @@ const gridChallenge = grid => {
   });
 
   return answer;
-};
-
-// imperative, mutative
-function gridChallenge(grid) {
-  let gridWithOrderedRows = [];
-
-  for (let i = 0; i < grid.length; i++) {
-    const row = grid[i];
-    const orderedRow = row
-      .split('')
-      .sort()
-      .join('');
-    gridWithOrderedRows.push(orderedRow);
-  }
-
-  let answer = 'YES';
-
-  for (let i = 0; i < gridWithOrderedRows.length; i++) {
-    const row = gridWithOrderedRows[i];
-    for (let j = 0; j < row.length; j++) {
-      const letter = row[j];
-      for (let k = i + 1; k < gridWithOrderedRows.length; k++) {
-        const otherRow = gridWithOrderedRows[k];
-        const otherLetter = otherRow[j];
-        if (otherLetter < letter) {
-          answer = 'NO';
-          break;
-        }
-      }
-    }
-  }
-
-  return answer;
-}
-
-// more declarative
-const gridChallenge = grid => {
-  const gridWithOrderedRows = grid.map(row =>
-    row
-      .split('')
-      .sort()
-      .join('')
-  );
-
-  return gridWithOrderedRows.reduce(
-    (answer, row, i) =>
-      answer === 'NO'
-        ? answer
-        : row.split('').reduce((localAnswer, letter, j) => {
-            if (localAnswer === 'NO') {
-              return localAnswer;
-            }
-
-            for (let k = i + 1; k < gridWithOrderedRows.length; k++) {
-              const otherRow = gridWithOrderedRows[k];
-              const otherLetter = otherRow[j];
-              if (otherLetter < letter) {
-                return 'NO';
-              }
-            }
-
-            return 'YES';
-          }, null),
-    null
-  );
 };
