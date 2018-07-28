0f72151e38577244f5fd0de712e0e2b8a6bbe0fb
CLooker <chad.looker@gmail.com>
2018-07-28 11:38:56 -0400

    make changes for readability

--- picking-numbers.js
+++ picking-numbers.js
@@ -1,52 +1,31 @@
 // https://www.hackerrank.com/challenges/picking-numbers/problem
 
-// declarative, immutable, implicit returns
-const pickingNumbers = a =>
-  a
-    .reduce(
-      ({ store, tempStore }, num, i) => (
-        Array.from(Array(a.length)).reduce(
-          (flag, _, j) =>
-            j !== i
-              ? (tempStore.forEach(
-                  n => Math.abs(n - a[j]) > 1 && (flag = false)
-                ),
-                flag === true && (tempStore = [...tempStore, a[j]]),
-                true)
-              : true,
-          true
-        ),
-        { store: [...store, tempStore], tempStore: [a[++i]] }
-      ),
-      { store: [], tempStore: [a[0]] }
-    )
-    .store.reduce((ans, item) => (item.length > ans ? item.length : ans), 0);
+const pickingNumbers = integers => {
+  let validSeqs = [];
 
-// imperative, mutable, explicit return
-function pickingNumbers(a) {
-  let store = [];
-  for (let i = 0; i < a.length; i++) {
-    let tempStore = [a[i]];
-    for (let j = 0; j < a.length; j++) {
-      if (i !== j) {
-        let flag = true;
-        for (let k = 0; k < tempStore.length; k++) {
-          if (Math.abs(tempStore[k] - a[j]) > 1) {
-            flag = false;
-          }
-        }
-        if (flag === true) {
-          tempStore.push(a[j]);
-        }
-      }
-    }
-    store.push(tempStore);
-  }
-  let ans = 0;
-  for (let i = 0; i < store.length; i++) {
-    if (store[i].length > ans) {
-      ans = store[i].length;
-    }
-  }
-  return ans;
-}
+  for (const [intIdx, integer] of integers.entries()) {
+    let validSeq = [integer];
+
+    for (const [otherIntIdx, otherInteger] of integers.entries()) {
+      const isSameIndex = intIdx === otherIntIdx;
+      if (isSameIndex) continue;
+
+      const isValidOtherInteger = validSeq.every(integer => {
+        const difference = Math.abs(integer - otherInteger);
+        const isValidDifference = difference <= 1;
+        return isValidDifference;
+      });
+      if (isValidOtherInteger) validSeq.push(otherInteger);
+    }
+
+    validSeqs.push(validSeq);
+  }
+
+  const longestValidSeq = validSeqs.reduce((longest, validSeq) => {
+    const { length } = validSeq;
+    const isLongest = length > longest;
+    return isLongest ? length : longest;
+  }, 0);
+
+  return longestValidSeq;
+};
