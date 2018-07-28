adc0b5bc991553cc73d4044863c779b99b678e77
CLooker <chad.looker@gmail.com>
2018-07-28 00:48:03 -0400

    make changes for readability

--- minimum-distances.js
+++ minimum-distances.js
@@ -1,40 +1,20 @@
 // https://www.hackerrank.com/challenges/minimum-distances/problem
 
-// mostly declarative, immutable, mostly implicit returns
-const minimumDistances = a => (
-  (coll = a.reduce((masterColl, aItem, i) => {
-    for (let j = i + 1; j < a.length; j++) {
-      a[j] === aItem && (masterColl = [...masterColl, Math.abs(j - i)]);
-    }
-    return masterColl;
-  }, [])),
-  (least = coll.reduce(
-    (least, num, i) => (num < least && (least = num), least),
-    coll[0]
-  )),
-  least > 0 ? least : -1
-);
+const minimumDistances = nums => {
+  const distances = [];
 
+  nums.forEach((num, i) => {
+    nums.forEach((otherNum, j) => {
+      const isSameIndex = i === j;
+      if (isSameIndex) return;
 
-// imperative, mutable, explicit return
-function minimumDistances(a) {
-  let coll = [];
-  for (let i = 0; i < a.length; i++) {
-    for (let j = i + 1; j < a.length; j++) {
-      if (a[j] === a[i]) {
-        coll.push(Math.abs(j - i));
+      const isEqual = num === otherNum;
+      if (isEqual) {
+        const distance = Math.abs(i - j);
+        distances.push(distance);
       }
-    }
-  }
-  let least = coll[0];
-  for (let i = 1; i < coll.length; i++) {
-    if (coll[i] < least) {
-      least = coll[i];
-    }
-  }
-  if (least > 0) {
-    return least;
-  } else {
-    return -1;
-  }
-}
+    });
+  });
+
+  return distances.length ? Math.min(...distances) : -1;
+};
