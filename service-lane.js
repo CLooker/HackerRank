fbc79e5aa655fe37f89012f23659d04662051fb6
CLooker <chad.looker@gmail.com>
2018-07-28 23:45:16 -0400

    use map

--- service-lane.js
+++ service-lane.js
@@ -1,19 +1,10 @@
 // https://www.hackerrank.com/challenges/service-lane/problem
 
-// NOTE: the first arg to serviceLane that hackerrank provides
-// when it actually invokes serviceLane is incorrect.
-// It must be `width`. You need to change it.
-
-function serviceLane(widths, widthsIntervals) {
-  let safeWidths = [];
-
-  widthsIntervals.forEach(interval => {
+const serviceLane = (widths, widthsIntervals) =>
+  widthsIntervals.map(interval => {
     const [start, exclusiveEnd] = interval;
     const end = exclusiveEnd + 1;
     const localWidths = widths.slice(start, end);
     const safeWidth = Math.min(...localWidths);
-    safeWidths.push(safeWidth);
+    return safeWidth;
   });
-
-  return safeWidths;
-}
