e94df9f91b6c6bdf0b09f29244f6e5922c128527
CLooker <chad.looker@gmail.com>
2018-07-29 01:12:44 -0400

    add changes for readability

--- strong-password.js
+++ strong-password.js
@@ -1,40 +1,36 @@
 // https://www.hackerrank.com/challenges/strong-password/problem
 
-function minimumNumber(n, password) {
+const getCharArrs = () => {
   const numbers = '0123456789';
   const lowerCase = 'abcdefghijklmnopqrstuvwxyz';
   const upperCase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
   const specialCharacters = '!@#$%^&*()-+';
-  const charArrs = [
-    [...numbers],
-    [...lowerCase],
-    [...upperCase],
-    [...specialCharacters]
-  ];
-  const passwordArr = [...password];
+  return [[...numbers], [...lowerCase], [...upperCase], [...specialCharacters]];
+};
 
-  const isLongEnough = (() => password.length >= 6)();
+function minimumNumber(n, password) {
+  const isLongEnough = password.length >= 6;
 
-  let typesOfCharMissing = 0;
+  const [isValid, typesOfCharMissing] = (() => {
+    let typesOfCharMissing = 0;
 
-  const hasCorrectChars = (() => {
-    for (const charArr of charArrs) {
-      const passesTest = passwordArr.some(char =>
-        charArr.some(otherChar => otherChar === char)
-      );
-      if (!passesTest) {
-        typesOfCharMissing++;
+    const hasCorrectChars = (() => {
+      const charArrs = getCharArrs();
+      for (const charArr of charArrs) {
+        const passesTest = [...password].some(char =>
+          charArr.some(otherChar => otherChar === char)
+        );
+        if (!passesTest) typesOfCharMissing++;
       }
-    }
 
-    return typesOfCharMissing === 0;
+      return typesOfCharMissing === 0;
+    })();
+
+    const isValid = isLongEnough && hasCorrectChars;
+    return [isValid, typesOfCharMissing];
   })();
 
-  const isValid = (() => isLongEnough && hasCorrectChars)();
-
-  if (isValid) {
-    return 0;
-  }
+  if (isValid) return 0;
 
   const minChangesToMake = (() => {
     const howManyCharShort = isLongEnough ? 0 : 6 - password.length;
