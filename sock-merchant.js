de8228f21ac2ab5d556c2e0b21cf4c8fa425256b
CLooker <chad.looker@gmail.com>
2018-07-29 00:14:15 -0400

    make changes for readability

--- sock-merchant.js
+++ sock-merchant.js
@@ -1,32 +1,23 @@
 // https://www.hackerrank.com/challenges/sock-merchant/problem
 
-// declarative
-const sockMerchant = (n, ar) =>
-  ar.reduce(
-    ({ total, j }, targetSock, i) => (
-      ar.forEach((_, k) => {
-        j === k &&
-          (ar[j] === targetSock
-            ? (++total, (ar[j] = NaN), (j = -1))
-            : j === -1 || ++j);
-      }),
-      { total, j: (i += 2) }
-    ),
-    { total: 0, j: 1 }
-  ).total;
+const sockMerchant = (socksTotal, colors) => {
+  let matchingColorPairs = 0;
 
-// imperative, mutable
-function sockMerchant(n, ar) {
-  let total = 0;
-  for (let i = 0; i < ar.length; i++) {
-    for (let j = i + 1; j < ar.length; j++) {
-      if (ar[j] === ar[i]) {
-        ++total;
-        ar[j] = NaN;
+  for (const [colorIdx, color] of colors.entries()) {
+    for (
+      let otherColorIdx = colorIdx + 1;
+      otherColorIdx < colors.length;
+      otherColorIdx++
+    ) {
+      const nextColor = colors[otherColorIdx];
+      const isMatch = color === nextColor;
+      if (isMatch) {
+        matchingColorPairs++;
+        colors.splice(otherColorIdx, 1);
         break;
       }
     }
   }
-  return total;
-}
 
+  return matchingColorPairs;
+};
