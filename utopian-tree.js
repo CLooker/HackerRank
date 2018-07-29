23d2bf1422c969070463644c26fa0db00ecee7c3
CLooker <chad.looker@gmail.com>
2018-07-29 01:43:20 -0400

    make changes for readability

--- utopian-tree.js
+++ utopian-tree.js
@@ -1,22 +1,10 @@
 // https://www.hackerrank.com/challenges/utopian-tree/problem
 
-// declarative, immutable, implicit return
-const utopianTree = n =>
-  n > 0
-    ? Array.from(Array(n))
-        .map((_, i) => ++i)
-        .reduce((h, i) => (i % 2 === 0 ? ++h : h * 2), 1)
-    : 1;
-
-// imperative, mutable, explicit return
-function utopianTree(n) {
+const utopianTree = growthCycleCount => {
   let height = 1;
-  for (let i = 1; i <= n; i++) {
-    if (i % 2 === 0) {
-      ++height;
-    } else {
-      height = height * 2;
-    }
+  for (let growthCycle = 1; growthCycle <= growthCycleCount; growthCycle++) {
+    const isEvenCycle = growthCycle % 2 === 0;
+    isEvenCycle ? height++ : (height *= 2);
   }
   return height;
-}
+};
