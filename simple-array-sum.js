8c5d8d215306653af2d3250a573bdb55a102234f
CLooker <chad.looker@gmail.com>
2018-07-28 23:55:47 -0400

    make changes for readability

--- simple-array-sum.js
+++ simple-array-sum.js
@@ -1,3 +1,4 @@
 // https://www.hackerrank.com/challenges/simple-array-sum/problem
 
-const simpleArraySum = (n, ar) => ar.reduce((total, n) => (total += n));
+const simpleArraySum = integers =>
+  integers.reduce((total, integer) => total + integer);
