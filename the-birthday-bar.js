5e45c812d1102423f47e9b3a4146864c3d23bcb3
CLooker <chad.looker@gmail.com>
2018-07-29 21:09:32 -0400

    add the-birthday-bar solution

--- the-birthday-bar.js
+++ the-birthday-bar.js
@@ -1,25 +1,19 @@
 // https://www.hackerrank.com/challenges/the-birthday-bar/problem
 
-const solve = (n, s, d, m) => (
-  s.reduce(({ result, tempSum }, item, i) => {
-    for (let j = i; j < i + m; j++) {
-      tempSum += s[j];
-    }
-    return tempSum === d ? {result: ++result, tempSum: 0} : {result, tempSum: 0};
-  }, {result: 0, tempSum: 0}).result
-)
+const solve = (chocNums, dayOfBirth, monthOfBirth) => {
+  const targetLength = monthOfBirth;
+  const targetSum = dayOfBirth;
 
-// imperative
-function solve(n, s, d, m) {
-  let result = 0;
-  for (let i = 0; i < s.length; i++) {
-    let tempSum = 0;
-    for (let j = i; j < i + m; j++) {
-      tempSum += s[j];
-    }
-    if (tempSum === d) {
-      ++result;
-    }
-  }
-  return result;
-}
+  const validChocNumSeqs = chocNums.filter((chocNum, chocNumIdx) => {
+    const sliceEnd = chocNumIdx + targetLength;
+    const chocNumSeq = chocNums.slice(chocNumIdx, sliceEnd);
+    const chocNumSeqTotal = chocNumSeq.reduce(
+      (total, chocNum) => total + chocNum,
+      0
+    );
+    const isValidSum = chocNumSeqTotal === targetSum;
+    return isValidSum;
+  });
+
+  return validChocNumSeqs.length;
+};
