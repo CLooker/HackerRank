44a86cce2f8d7305097b763810060c8baf492411
CLooker <chad.looker@gmail.com>
2018-08-11 23:05:33 -0400

    make changes for readability

--- divisible-sum-pairs.js
+++ divisible-sum-pairs.js
@@ -1,16 +1,16 @@
 // https://www.hackerrank.com/challenges/divisible-sum-pairs/problem
 
 const divisibleSumPairs = (_, divisor, nums) => {
-  let total = 0;
+  let divisibleSumPairTotal = 0;
 
   nums.forEach((num, i) => {
     const nextNums = nums.slice(i + 1);
     nextNums.forEach(nextNum => {
       const sumPair = num + nextNum;
       const isDivisibleSumPair = sumPair % divisor === 0;
-      if (isDivisibleSumPair) total++;
+      if (isDivisibleSumPair) divisibleSumPairTotal++;
     });
   });
 
-  return total;
+  return divisibleSumPairTotal;
 };
