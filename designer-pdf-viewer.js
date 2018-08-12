214a02f15a5a4a3fa46f3099928f0d28c9df2632
CLooker <chad.looker@gmail.com>
2018-08-11 23:04:11 -0400

    make changes for readability

--- designer-pdf-viewer.js
+++ designer-pdf-viewer.js
@@ -1,30 +1,18 @@
 // https://www.hackerrank.com/challenges/designer-pdf-viewer/problem
 
-function designerPdfViewer(h, word) {
+const designerPdfViewer = (alphabetHeights, word) => {
   const alphabet = [...'abcdefghijklmnopqrstuvwxyz'];
 
-  const abcIdxes = alphabet.reduce((idxes, letter, i) => {
-    const isLetterIncluded = word.includes(letter);
-    return isLetterIncluded ? idxes.concat(i) : idxes;
-  }, []);
+  let tallest = 0;
 
-  const hIdxes = abcIdxes.map(abcIdx => {
-    const hIdx = h[abcIdx];
-    return hIdx;
+  [...word].forEach(wordLetter => {
+    const alphabetHeightIndex = alphabet.indexOf(wordLetter);
+    const wordLetterHeight = alphabetHeights[alphabetHeightIndex];
+    const isTallest = wordLetterHeight > tallest;
+    if (isTallest) tallest = wordLetterHeight;
   });
 
-  const hIdxesSorted = [...hIdxes].sort((a, b) => b - a);
-  const [height] = hIdxesSorted;
   const width = word.length;
-  return height * width;
-}
 
-// declarative, immutable, implicit return
-const designerPdfViewer = (h, word) => (
-  (height = 'abcdefghijklmnopqrstuvwxyz'
-    .split('')
-    .map((item, index) => word.includes(item) && index)
-    .map(idx => h[idx])
-    .sort((a, b) => b - a)[0]),
-  height * word.length
-);
+  return tallest * width;
+};
