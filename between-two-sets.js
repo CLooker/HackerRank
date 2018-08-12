773e2c1be69edfb3ff5c54d2f2b1ef94e2b030b8
CLooker <chad.looker@gmail.com>
2018-08-11 21:43:16 -0400

    make change for readability

--- between-two-sets.js
+++ between-two-sets.js
@@ -2,33 +2,29 @@
 
 // (leftNums: Number[], rightNums: Number[]) -> Number[]
 const getTotalX = (leftNums, rightNums) => {
-  const middleNums = (() => {
-    const firstPossibleMiddleNum = leftNums[leftNums.length - 1];
-    const [lastPossibleMiddleNum] = rightNums;
+  const firstPossibleMiddleNum = leftNums[leftNums.length - 1];
+  const [lastPossibleMiddleNum] = rightNums;
 
-    let middleNums = [];
+  let middleNums = [];
 
-    for (
-      let num = firstPossibleMiddleNum;
-      num <= lastPossibleMiddleNum;
-      num += firstPossibleMiddleNum
-    ) {
-      const isNumEvenlyDivisbleByAllLeftNums = leftNums.every(
-        leftNum => num % leftNum === 0
-      );
+  for (
+    let num = firstPossibleMiddleNum;
+    num <= lastPossibleMiddleNum;
+    num += firstPossibleMiddleNum
+  ) {
+    const isNumEvenlyDivisbleByAllLeftNums = leftNums.every(
+      leftNum => num % leftNum === 0
+    );
 
-      const isNumAFactorForAllRightNums = rightNums.every(
-        rightNum => rightNum % num === 0
-      );
+    const isNumAFactorForAllRightNums = rightNums.every(
+      rightNum => rightNum % num === 0
+    );
 
-      const isMiddleNum =
-        isNumEvenlyDivisbleByAllLeftNums && isNumAFactorForAllRightNums;
+    const isMiddleNum =
+      isNumEvenlyDivisbleByAllLeftNums && isNumAFactorForAllRightNums;
 
-      if (isMiddleNum) middleNums = middleNums.concat(num);
-    }
-
-    return middleNums;
-  })();
+    if (isMiddleNum) middleNums = middleNums.concat(num);
+  }
 
   return middleNums.length;
 };
