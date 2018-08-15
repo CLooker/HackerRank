f8f106ca5deffe72f137dad5c5bd97a1a77d2dcb
CLooker <chad.looker@gmail.com>
2018-08-15 19:47:48 -0400

    add different version that uses array methods

--- grid-challenge.js
+++ grid-challenge.js
@@ -1,17 +1,35 @@
 // https://www.hackerrank.com/challenges/grid-challenge/problem?h_r=next-challenge&h_v=zen
 
 const gridChallenge = grid => {
-  const gridWithOrderedRows = grid.map(seq =>
-    seq
-      .split('')
-      .sort()
-      .join('')
-  );
+  const gridWithOrderedRows = grid.map(seq => [...seq].sort().join(''));
+
+  const isAllColsAscOrder = gridWithOrderedRows.every((row, rowIdx) => {
+    const isColAscOrder = [...row].every((rowLetter, rowLetterIdx) => {
+      const laterRows = gridWithOrderedRows.slice(rowIdx + 1);
+
+      const isColLetterAscOrder = laterRows.every(laterRow => {
+        const colLetter = laterRow[rowLetterIdx];
+        return rowLetter <= colLetter;
+      });
+
+      return isColLetterAscOrder;
+    });
+
+    return isColAscOrder;
+  });
+
+  return isAllColsAscOrder ? 'YES' : 'NO';
+};
+
+// another version
+
+const gridChallenge = grid => {
+  const gridWithOrderedRows = grid.map(seq => [...seq].sort().join(''));
 
   let answer = 'YES';
 
   gridWithOrderedRows.forEach((row, rowIdx) => {
-    row.split('').forEach((letter, letterIdx) => {
+    [...row].forEach((letter, letterIdx) => {
       const otherRows = gridWithOrderedRows.slice(rowIdx + 1);
       for (const otherRow of otherRows) {
         const otherLetter = otherRow[letterIdx];
