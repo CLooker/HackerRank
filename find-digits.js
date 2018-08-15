0b6208fdd0aa61f84bc911441ed676538195b7ab
CLooker <chad.looker@gmail.com>
2018-08-15 18:55:23 -0400

    make changes for readability

--- find-digits.js
+++ find-digits.js
@@ -1,8 +1,19 @@
 // https://www.hackerrank.com/challenges/find-digits/problem
 
-const findDigits = n =>
-  n
-    .toString()
-    .split("")
-    .map(digit => (n % digit === 0 ? 1 : 0))
-    .reduce((sum, item) => (sum += item));
+const findDigits = integer => {
+  const integerStr = String(integer);
+
+  const subIntFactorsOfInteger = [...integerStr].filter(subIntStr => {
+    const subInt = Number(subIntStr);
+    const isSubIntFactorOfInteger = integer % subInt === 0;
+    return isSubIntFactorOfInteger;
+  });
+
+  return subIntFactorsOfInteger.length;
+};
+
+const findDigits = integer =>
+  integer
+    .toString()
+    .split('') // array of subInts
+    .filter(subInt => integer % subInt === 0).length; // return only factors
