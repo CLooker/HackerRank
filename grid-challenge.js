4eb73a6ab622bb3a5e94596c746ece7d7802ce95
CLooker <chad.looker@gmail.com>
2018-08-15 21:07:23 -0400

    add superior solution

--- grid-challenge.js
+++ grid-challenge.js
@@ -1,45 +1,16 @@
 // https://www.hackerrank.com/challenges/grid-challenge/problem?h_r=next-challenge&h_v=zen
 
 const gridChallenge = grid => {
-  const gridWithOrderedRows = grid.map(seq => [...seq].sort().join(''));
+  const gridRowsAscOrder = grid.map(seq => [...seq].sort());
 
-  const isAllColsAscOrder = gridWithOrderedRows.every((row, rowIdx) => {
-    const isColAscOrder = [...row].every((rowLetter, rowLetterIdx) => {
-      const laterRows = gridWithOrderedRows.slice(rowIdx + 1);
+  const [colFirstLetters, ...rest] = gridRowsAscOrder;
 
-      const isColLetterAscOrder = laterRows.every(laterRow => {
-        const colLetter = laterRow[rowLetterIdx];
-        return rowLetter <= colLetter;
-      });
-
-      return isColLetterAscOrder;
-    });
-
+  const isAllColsAscOrder = colFirstLetters.every((colFirstLetter, colIdx) => {
+    const restOfCol = rest.map(rowAscOrder => rowAscOrder[colIdx]);
+    const col = [colFirstLetter, ...restOfCol];
+    const isColAscOrder = col.every(colLetter => colFirstLetter <= colLetter);
     return isColAscOrder;
   });
 
   return isAllColsAscOrder ? 'YES' : 'NO';
-};
-
-// another version
-
-const gridChallenge = grid => {
-  const gridWithOrderedRows = grid.map(seq => [...seq].sort().join(''));
-
-  let answer = 'YES';
-
-  gridWithOrderedRows.forEach((row, rowIdx) => {
-    [...row].forEach((letter, letterIdx) => {
-      const otherRows = gridWithOrderedRows.slice(rowIdx + 1);
-      for (const otherRow of otherRows) {
-        const otherLetter = otherRow[letterIdx];
-        if (otherLetter < letter) {
-          answer = 'NO';
-          break;
-        }
-      }
-    });
-  });
-
-  return answer;
 };
