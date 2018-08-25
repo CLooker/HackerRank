c4ace13fb815fe00d712126ade9f56991188e293
CLooker <chad.looker@gmail.com>
2018-08-25 13:05:16 -0400

    add minimal code solution, and improved original solution

--- picking-numbers.js
+++ picking-numbers.js
@@ -1,31 +1,52 @@
 // https://www.hackerrank.com/challenges/picking-numbers/problem
 
-const pickingNumbers = integers => {
-  let validSeqs = [];
+// more explicit
+const pickingNumbers = nums => {
+  const validNumSeqs = nums.map((num, numIdx) => {
+    const validNumSeq = [num];
 
-  for (const [intIdx, integer] of integers.entries()) {
-    let validSeq = [integer];
+    nums.forEach((otherNum, otherNumIdx) => {
+      if (numIdx === otherNumIdx) return;
 
-    for (const [otherIntIdx, otherInteger] of integers.entries()) {
-      const isSameIndex = intIdx === otherIntIdx;
-      if (isSameIndex) continue;
-
-      const isValidOtherInteger = validSeq.every(integer => {
-        const difference = Math.abs(integer - otherInteger);
-        const isValidDifference = difference <= 1;
-        return isValidDifference;
+      const isValidNum = validNumSeq.every(validNum => {
+        const difference = Math.abs(validNum - otherNum);
+        return difference <= 1;
       });
-      if (isValidOtherInteger) validSeq.push(otherInteger);
-    }
 
-    validSeqs.push(validSeq);
-  }
+      if (isValidNum) validNumSeq.push(otherNum);
+    });
 
-  const longestValidSeq = validSeqs.reduce((longest, validSeq) => {
-    const { length } = validSeq;
+    return validNumSeq;
+  });
+
+  const longest = validNumSeqs.reduce((longest, validNumSeq) => {
+    const { length } = validNumSeq;
     const isLongest = length > longest;
     return isLongest ? length : longest;
   }, 0);
 
-  return longestValidSeq;
+  return longest;
 };
+
+// minimal code
+const pickingNumbers = nums =>
+  nums
+    // returns array of `validNums` arrays
+    .map((num, numIdx) =>
+      nums.reduce(
+        (validNums, otherNum, otherNumIdx) => {
+          if (numIdx === otherNumIdx) return validNums; // skip same member in `nums`
+          const isValidNum = validNums.every(
+            validNum => Math.abs(validNum - otherNum) <= 1
+          );
+          return isValidNum ? validNums.concat(otherNum) : validNums;
+        },
+        [num] // start each validNums array with `num`
+      )
+    )
+    // returns the longest of all the `validNums` arrays
+    .reduce(
+      (longest, validNums) =>
+        validNums.length > longest ? validNums.length : longest,
+      0
+    );
