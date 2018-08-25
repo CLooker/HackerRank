0f4ec57130f157b6f1ad3804db6c296eb3ddf669
CLooker <chad.looker@gmail.com>
2018-08-25 12:16:39 -0400

    make use of default paramater spread operation and some other simplifiers

--- mars-exploration.js
+++ mars-exploration.js
@@ -1,40 +1,31 @@
 // https://www.hackerrank.com/challenges/mars-exploration/problem
 
-function marsExploration(message) {
+function marsExploration([...message]) {
   const possibleSOSMessages = (() => {
-    let messageArr = [...message];
     let arr = [];
 
-    for (let messageIdx = 0; messageIdx < messageArr.length; messageIdx += 3) {
+    for (let messageIdx = 0; messageIdx < message.length; messageIdx += 3) {
       const sliceStart = messageIdx;
       const sliceEnd = messageIdx + 3;
-      const possibleSOSMessage = messageArr
-        .slice(sliceStart, sliceEnd)
-        .join('');
+      const possibleSOSMessage = message.slice(sliceStart, sliceEnd).join('');
       arr.push(possibleSOSMessage);
     }
 
     return arr;
   })();
 
-  const radiationLetterTotal = (() => {
-    const SOS = 'SOS';
+  return possibleSOSMessages.reduce(
+    (radiationLetterTotal, possibleSOSMessage) => {
+      let localRadiationLetterTotal = 0;
 
-    return possibleSOSMessages.reduce(
-      (radiationLetterTotal, possibleSOSMessage) => {
-        let localRadiationLetterTotal = 0;
+      [...possibleSOSMessage].forEach((letter, letterIdx) => {
+        const SOSLetter = 'SOS'[letterIdx];
+        const isRadiationLetter = letter !== SOSLetter;
+        if (isRadiationLetter) localRadiationLetterTotal++;
+      });
 
-        [...possibleSOSMessage].forEach((letter, letterIdx) => {
-          const SOSLetter = SOS[letterIdx];
-          const isRadiationLetter = letter !== SOSLetter;
-          if (isRadiationLetter) localRadiationLetterTotal++;
-        });
-
-        return radiationLetterTotal + localRadiationLetterTotal;
-      },
-      0
-    );
-  })();
-
-  return radiationLetterTotal;
+      return radiationLetterTotal + localRadiationLetterTotal;
+    },
+    0
+  );
 }
