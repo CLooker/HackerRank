881001b6a09b0367a9e752a07f5ff0094d5caea1
CLooker <chad.looker@gmail.com>
2018-11-20 12:52:27 -0500

    update solution

--- hackerrank-in-a-string.js
+++ hackerrank-in-a-string.js
@@ -1,20 +1,17 @@
-// https://www.hackerrank.com/challenges/hackerrank-in-a-string/submissions/code/85148296
+// https://www.hackerrank.com/challenges/hackerrank-in-a-string/
 
-const hackerrankInString = ([...str]) => {
+const hackerrankInString = ([...letters]) => {
   const HR = 'hackerrank';
   let mutableHR = HR;
   let possibleHR = '';
 
-  return (() => {
-    for (const letter of str) {
-      const [nextHRletter] = mutableHR;
-      if (letter === nextHRletter) {
-        possibleHR = possibleHR.concat(letter);
-        mutableHR = mutableHR.slice(1);
-        if (possibleHR === HR) return 'YES';
-      }
-    }
+  for (const letter of letters) {
+    const [nextHRletter] = mutableHR;
+    if (letter !== nextHRletter) continue;
+    possibleHR = possibleHR.concat(letter);
+    mutableHR = mutableHR.slice(1);
+    if (possibleHR === HR) return 'YES';
+  }
 
-    return 'NO';
-  })();
+  return 'NO';
 };
