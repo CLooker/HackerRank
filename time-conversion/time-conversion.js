c59aff180908f5102e1c443d300e2b638fe6d8b3
CLooker <chad.looker@gmail.com>
2019-06-14 19:10:28 -0400

    Update js solution

--- time-conversion/time-conversion.js
+++ time-conversion/time-conversion.js
@@ -1,26 +1,24 @@
 // https://www.hackerrank.com/challenges/time-conversion/problem
 
 const timeConversion = time => {
-  let timeToAlter = parseInt(time.slice(0, 2));
-  let timeToKeep = time.slice(3, time.length - 2);
+  const [unformattedHH, mm, ssWithPeriod] = time.split(':');
 
-  const isPM = time.match(/P/g);
-  if (isPM) {
-    const isMilitaryFormat = timeToAlter >= 12;
-    if (!isMilitaryFormat) timeToAlter += 12;
-  }
+  const hh = (() => {
+    const isPm = ssWithPeriod.includes('PM');
 
-  const isAM = time.match(/A/g);
-  if (isAM) {
-    const doesNeedLeadingZeros = timeToAlter === 12;
-    if (doesNeedLeadingZeros) {
-      timeToAlter = '00';
-    } else {
-      const doesNeedLeadingZero = timeToAlter < 10;
-      if (doesNeedLeadingZero) timeToAlter = `0${timeToAlter}`;
+    const periodConversion = isPm ? 12 + +unformattedHH : unformattedHH;
+
+    switch (periodConversion) {
+      case 24:
+        return '12';
+      case '12':
+        return '00';
+      default:
+        return periodConversion;
     }
-  }
+  })();
 
-  const convertedTime = `${timeToAlter}:${timeToKeep}`;
-  return convertedTime;
+  const ss = ssWithPeriod.slice(0, 2);
+
+  return `${hh}:${mm}:${ss}`;
 };
