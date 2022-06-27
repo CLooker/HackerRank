94c87b434762305dbe680bee46ebb7d6e2acf947
Chad Looker <chad.looker@gmail.com>
2022-06-27 19:59:45 -0400

    Improve mini-max-sum.go

--- mini-max-sum/mini-max-sum.go
+++ mini-max-sum/mini-max-sum.go
@@ -6,7 +6,9 @@ import (
 )
 
 func miniMaxSum(arr []int32) {
-	var sums []uint64
+	var sumMin uint64 = math.MaxUint64
+	var sumMax uint64 = 0
+
 	for i := 0; i < len(arr); i++ {
 		var sum uint64 = 0
 		for j, n := range arr {
@@ -14,20 +16,15 @@ func miniMaxSum(arr []int32) {
 				sum += uint64(n)
 			}
 		}
-		sums = append(sums, sum)
-	}
 
-	var min uint64 = math.MaxUint64
-	var max uint64 = 0
-	for _, sum := range sums {
-		if sum < min {
-			min = sum
+		if sum < sumMin {
+			sumMin = sum
 		}
 
-		if sum > max {
-			max = sum
+		if sum > sumMax {
+			sumMax = sum
 		}
 	}
 
-	fmt.Println(min, max)
+	fmt.Println(sumMin, sumMax)
 }
