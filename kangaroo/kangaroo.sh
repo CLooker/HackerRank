052396df2187500dc4cfc5dc9e46e66367d91c25
Chad Looker <chad.looker@gmail.com>
2022-07-03 11:46:49 -0400

    Improve kangaroo.sh read command usage

--- kangaroo/kangaroo.sh
+++ kangaroo/kangaroo.sh
@@ -1,15 +1,10 @@
 #!/bin/bash
 
-read args
-
-a_pos=$(echo $args | cut -d ' ' -f 1)
-a_jump_size=$(echo $args | cut -d ' ' -f 2)
-b_pos=$(echo $args | cut -d ' ' -f 3)
-b_jump_size=$(echo $args | cut -d ' ' -f 4)
+read a_pos a_jump_size b_pos b_jump_size
 
 behind_jump_size=
 ahead_jump_size=
-if (( a_pos <= b_pos )); then
+if ((a_pos <= b_pos)); then
   behind_jump_size=$a_jump_size
   ahead_jump_size=$b_jump_size
 else
@@ -17,14 +12,14 @@ else
   ahead_jump_size=$a_jump_size
 fi
 
-if (( ahead_jump_size >= behind_jump_size )); then
+if ((ahead_jump_size >= behind_jump_size)); then
   echo 'NO'
   exit 0
 fi
 
 behind_pos=
 ahead_pos=
-if (( a_pos <= b_pos )); then
+if ((a_pos <= b_pos)); then
   behind_pos=$a_pos
   ahead_pos=$b_pos
 else
@@ -33,15 +28,15 @@ else
 fi
 
 while true; do
-  if (( ahead_pos <= behind_pos )); then
-    if (( ahead_pos == behind_pos )); then
+  if ((ahead_pos <= behind_pos)); then
+    if ((ahead_pos == behind_pos)); then
         echo 'YES'
     else
         echo 'NO'
     fi
     break
   else
-    behind_pos=$(( behind_pos + behind_jump_size ))
-    ahead_pos=$(( ahead_pos + ahead_jump_size ))
+    behind_pos=$((behind_pos + behind_jump_size))
+    ahead_pos=$((ahead_pos + ahead_jump_size))
   fi
 done
