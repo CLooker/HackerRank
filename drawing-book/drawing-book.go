dbae49f7f1a469ea8541ea4e7d6433453b452a50
Chad Looker <chad.looker@gmail.com>
2022-07-05 09:45:06 -0400

    Fix drawing-book.go

--- drawing-book/drawing-book.go
+++ drawing-book/drawing-book.go
@@ -3,12 +3,14 @@ package drawingbook
 import "math"
 
 func pageCount(pageMax int32, pageTarget int32) int32 {
-	a := pageMax - pageTarget
-	b := pageTarget - 1
+	fromStart := pageTarget - 1
+	fromEnd := pageMax - pageTarget
 
-	if a <= b {
-		return a / 2
+	if fromStart < fromEnd {
+		return int32(math.Ceil(float64(fromStart) / float64(2)))
+	} else if (pageMax-pageTarget == 1) && (pageMax%2 == 0) {
+		return (fromEnd / 2) + 1
 	} else {
-		return int32(math.Ceil(float64(b) / float64(2)))
+		return fromEnd / 2
 	}
 }
