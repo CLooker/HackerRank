0e3ed5eb3a4f1088a86fd9cd88153925789da256
Chad Looker <chad.looker@gmail.com>
2022-07-16 10:52:22 -0400

    Improve bigger-is-greater.js

--- bigger-is-greater/bigger-is-greater.js
+++ bigger-is-greater/bigger-is-greater.js
@@ -1,20 +1,29 @@
-const biggerIsGreater = (str) => {
-  const NO_ANSWER = 'no answer';
-  let chars = [...str];
-  const i = chars.reduce(
-    (accum, char, idx) => (idx === 0 || chars[idx - 1] >= char ? accum : idx),
-    -1
-  );
-  if (i === -1) return NO_ANSWER;
-  const pivot = chars[i - 1];
-  const j = chars.reduce(
-    (accum, char, idx) => (idx >= i && char > pivot ? idx : accum),
-    -1
-  );
-  chars[i - 1] = chars[j];
-  chars[j] = pivot;
-  const head = chars.slice(0, i);
-  const tail = chars.slice(i).reverse();
-  const ret = [...head, ...tail].join('');
-  return ret === str ? NO_ANSWER : ret;
+const biggerIsGreater = word => {
+  const chars = [ ...word ];
+
+  const pivotIdx = chars
+    .slice(1)
+    .reduce((acc, ch, i) => ch > chars[i] ? i : acc, -1);
+
+  if (pivotIdx === -1) {
+    return 'no answer';
+  }
+
+  const pivot = chars[pivotIdx];
+
+  const pivotIdxNext = chars
+    .slice(pivotIdx)
+    .reduce((acc, ch, i) => ch > pivot ? i + pivotIdx : acc, -1);
+
+  chars[pivotIdx] = chars[pivotIdxNext];
+  chars[pivotIdxNext] = pivot;
+
+  const result = [
+    ...chars.slice(0, pivotIdx + 1),
+    ...chars.slice(pivotIdx + 1).reverse()
+  ].join('');
+
+  return result === word
+    ? 'no answer'
+    : result;
 };
