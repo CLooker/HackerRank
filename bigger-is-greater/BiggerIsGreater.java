1496f3bf2751ab0412c9fad8ca0eaf01764d045c
Chad Looker <chad.looker@gmail.com>
2022-07-16 09:26:25 -0400

    Add BiggerIsGreater.java

---
+++ bigger-is-greater/BiggerIsGreater.java
@@ -0,0 +1,44 @@
+public class BiggerIsGreater {
+
+  public static String biggerIsGreater(String word) {
+    char[] chars = word.toCharArray();
+
+    int pivotIdx = -1;
+    for (int i = 1; i < chars.length; i++) {
+      char ch = chars[i];
+      char charPrev = chars[i - 1];
+      if (ch > charPrev) {
+        pivotIdx = i - 1;
+      } 
+    }
+
+    if (pivotIdx == -1) {
+      return "no answer";
+    }
+
+    char pivot = chars[pivotIdx];
+    int pivotIdxNext = -1;
+    for (int i = pivotIdx; i < chars.length; i++) {
+      if (chars[i] > pivot) {
+        pivotIdxNext = i;
+      }
+    } 
+
+    
+    chars[pivotIdx] = chars[pivotIdxNext];
+    chars[pivotIdxNext] = pivot;
+
+    String result = "";
+    for (int i = 0; i <= pivotIdx; i++) {
+      result += chars[i];
+    }
+    for (int i = chars.length - 1; i > pivotIdx; i--) {
+      result += chars[i];
+    }
+
+    return result.equals(word)
+      ? "no answer"
+      : result;
+  }
+
+}
